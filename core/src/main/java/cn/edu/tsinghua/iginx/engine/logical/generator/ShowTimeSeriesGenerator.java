1fd63d1c8394f6de9cd97663272d69acd4ee9c4f
RemHero <1104304963@qq.com>
2023-02-17 20:22:33 +0800

    remove some notes

--- core/src/main/java/cn/edu/tsinghua/iginx/engine/logical/generator/ShowTimeSeriesGenerator.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/engine/logical/generator/ShowTimeSeriesGenerator.java
@@ -1,40 +1,13 @@
 package cn.edu.tsinghua.iginx.engine.logical.generator;
 
-import cn.edu.tsinghua.iginx.conf.ConfigDescriptor;
-import cn.edu.tsinghua.iginx.engine.logical.utils.OperatorUtils;
-import cn.edu.tsinghua.iginx.engine.logical.utils.PathUtils;
-import cn.edu.tsinghua.iginx.engine.shared.operator.AddSchemaPrefix;
 import cn.edu.tsinghua.iginx.engine.shared.operator.Operator;
-import cn.edu.tsinghua.iginx.engine.shared.operator.Project;
 import cn.edu.tsinghua.iginx.engine.shared.operator.ShowTimeSeries;
-import cn.edu.tsinghua.iginx.engine.shared.source.FragmentSource;
 import cn.edu.tsinghua.iginx.engine.shared.source.GlobalSource;
-import cn.edu.tsinghua.iginx.engine.shared.source.OperatorSource;
-import cn.edu.tsinghua.iginx.metadata.DefaultMetaManager;
-import cn.edu.tsinghua.iginx.metadata.IMetaManager;
-import cn.edu.tsinghua.iginx.metadata.entity.*;
-import cn.edu.tsinghua.iginx.policy.IPolicy;
-import cn.edu.tsinghua.iginx.policy.PolicyManager;
-import cn.edu.tsinghua.iginx.sql.statement.SelectStatement;
 import cn.edu.tsinghua.iginx.sql.statement.ShowTimeSeriesStatement;
 import cn.edu.tsinghua.iginx.sql.statement.Statement;
-import cn.edu.tsinghua.iginx.utils.Pair;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import static cn.edu.tsinghua.iginx.metadata.utils.FragmentUtils.keyFromTSIntervalToTimeInterval;
 
 public class ShowTimeSeriesGenerator extends AbstractGenerator {
-
-    private static final Logger logger = LoggerFactory.getLogger(ShowTimeSeriesGenerator.class);
     private final static ShowTimeSeriesGenerator instance = new ShowTimeSeriesGenerator();
-    private final static IMetaManager metaManager = DefaultMetaManager.getInstance();
-    private final IPolicy policy = PolicyManager.getInstance()
-            .getPolicy(ConfigDescriptor.getInstance().getConfig().getPolicyClassName());
 
     private ShowTimeSeriesGenerator() {
         this.type = GeneratorType.ShowTimeSeries;
@@ -42,17 +15,6 @@ public class ShowTimeSeriesGenerator extends AbstractGenerator {
 
     public static ShowTimeSeriesGenerator getInstance() {
         return instance;
-    }
-
-    private Pair<Map<TimeInterval, List<FragmentMeta>>, List<FragmentMeta>> getFragmentsByTSInterval(TimeSeriesInterval interval) {
-        Map<TimeSeriesRange, List<FragmentMeta>> fragmentsByTSInterval = metaManager.getFragmentMapByTimeSeriesInterval(PathUtils.trimTimeSeriesInterval(interval), true);
-//        if (!metaManager.hasFragment()) {
-//            //on startup
-//            Pair<List<FragmentMeta>, List<StorageUnitMeta>> fragmentsAndStorageUnits = policy.generateInitialFragmentsAndStorageUnits(selectStatement);
-//            metaManager.createInitialFragmentsAndStorageUnits(fragmentsAndStorageUnits.v, fragmentsAndStorageUnits.k);
-//            fragmentsByTSInterval = metaManager.getFragmentMapByTimeSeriesInterval(interval, true);
-//        }
-        return keyFromTSIntervalToTimeInterval(fragmentsByTSInterval);
     }
 
     @Override
