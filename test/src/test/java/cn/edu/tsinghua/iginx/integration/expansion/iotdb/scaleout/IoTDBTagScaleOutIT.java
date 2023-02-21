972c50b044e63d7ecfbb31b008c6fea2559471da
RemHero <1104304963@qq.com>
2023-02-21 16:50:36 +0800

    rename to make the work clear

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/iotdb/scaleout/IoTDBTagScaleOutIT.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/iotdb/scaleout/IoTDBTagScaleOutIT.java
@@ -1,14 +1,9 @@
 package cn.edu.tsinghua.iginx.integration.expansion.iotdb.scaleout;
 
-import cn.edu.tsinghua.iginx.exceptions.ExecutionException;
-import cn.edu.tsinghua.iginx.exceptions.SessionException;
-import cn.edu.tsinghua.iginx.integration.SQLSessionIT;
 import cn.edu.tsinghua.iginx.integration.TagIT;
 import cn.edu.tsinghua.iginx.utils.FileReader;
-import org.junit.Test;
 
 public class IoTDBTagScaleOutIT extends TagIT {
-
     public IoTDBTagScaleOutIT() {
         super();
         this.storageEngineType = FileReader.convertToString("./src/test/java/cn/edu/tsinghua/iginx/integration/DBConf.txt");
