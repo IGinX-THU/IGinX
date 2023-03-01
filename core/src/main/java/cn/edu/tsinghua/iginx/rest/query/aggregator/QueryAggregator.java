5a0459a3ec187a82faff880a469d88dcd8115c42
YouJiacheng <1503679330@qq.com>
2023-03-01 17:26:38 +0800

    Rest simplification 2 (#18)
    * Remove unused imports
    
    * Simplify Rest Query Aggregator
    
    ---------
    
    Co-authored-by: Yuqing Zhu <yuqing.zhu@139.com>

--- core/src/main/java/cn/edu/tsinghua/iginx/rest/query/aggregator/QueryAggregator.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/rest/query/aggregator/QueryAggregator.java
@@ -19,11 +19,9 @@
 package cn.edu.tsinghua.iginx.rest.query.aggregator;
 
 import cn.edu.tsinghua.iginx.rest.RestSession;
-import cn.edu.tsinghua.iginx.rest.RestUtils;
 import cn.edu.tsinghua.iginx.rest.bean.QueryResultDataset;
 import cn.edu.tsinghua.iginx.session.SessionQueryDataSet;
 import cn.edu.tsinghua.iginx.thrift.AggregateType;
-import cn.edu.tsinghua.iginx.thrift.DataType;
 import cn.edu.tsinghua.iginx.thrift.TimePrecision;
 import cn.edu.tsinghua.iginx.utils.TimeUtils;
 
@@ -39,10 +37,15 @@ public abstract class QueryAggregator {
     private String metric_name;
     private Filter filter;
     private QueryAggregatorType type;
-
+    private AggregateType aggregateType;
 
     protected QueryAggregator(QueryAggregatorType type) {
+        this(type, null);
+    }
+
+    protected QueryAggregator(QueryAggregatorType type, AggregateType aggregateType) {
         this.type = type;
+        this.aggregateType = aggregateType;
     }
 
     public Double getDivisor() {
@@ -106,8 +109,23 @@ public abstract class QueryAggregator {
     }
 
     public QueryResultDataset doAggregate(RestSession session, List<String> paths, Map<String, List<String>> tagList, long startTimestamp, long endTimestamp, TimePrecision timePrecision) {
+        SessionQueryDataSet sessionQueryDataSet = null;
+        try {
+            if (type == QueryAggregatorType.NONE) {
+                sessionQueryDataSet = session.queryData(paths, startTimestamp, endTimestamp, tagList, timePrecision);
+            } else if (aggregateType != null) {
+                sessionQueryDataSet = session.downsampleQuery(paths, tagList, startTimestamp, endTimestamp, aggregateType, getDur(), timePrecision);
+            }
+        } catch (Exception e) {
+            // TODO: more precise exception catch
+            e.printStackTrace();
+            return new QueryResultDataset();
+        }
+        if (sessionQueryDataSet == null) {
+            // type != QueryAggregatorType.NONE) and aggregateType == null
+            throw new RuntimeException("Unsupported Query");
+        }
         QueryResultDataset queryResultDataset = new QueryResultDataset();
-        SessionQueryDataSet sessionQueryDataSet = session.queryData(paths, startTimestamp, endTimestamp, tagList, timePrecision);
         queryResultDataset.setPaths(getPathsFromSessionQueryDataSet(sessionQueryDataSet));
         int n = sessionQueryDataSet.getKeys().length;
         int m = sessionQueryDataSet.getPaths().size();
@@ -118,50 +136,16 @@ public abstract class QueryAggregator {
             for (int i = 0; i < n; i++) {
                 if (sessionQueryDataSet.getValues().get(i).get(j) != null) {
                     value.add(sessionQueryDataSet.getValues().get(i).get(j));
-//                    long timeRes = TimeUtils.getTimeFromNsToSpecPrecision(sessionQueryDataSet.getTimestamps()[i], TimeUtils.DEFAULT_TIMESTAMP_PRECISION);
                     long timeRes = sessionQueryDataSet.getKeys()[i];
                     time.add(timeRes);
                     queryResultDataset.add(timeRes, sessionQueryDataSet.getValues().get(i).get(j));
                     datapoints += 1;
                 }
             }
-            if(!value.isEmpty())
-                queryResultDataset.addValueLists(value);
-            if(!time.isEmpty())
-                queryResultDataset.addTimeLists(time);
+            queryResultDataset.addValueLists(value);
+            queryResultDataset.addTimeLists(time);
         }
         queryResultDataset.setSampleSize(datapoints);
-        return queryResultDataset;
-    }
-
-    public QueryResultDataset doAggregateWithDownsampleQuery(RestSession session, AggregateType aggregateType, List<String> paths, Map<String, List<String>> tagList, long startTimestamp, long endTimestamp, TimePrecision timePrecision) {
-        QueryResultDataset queryResultDataset = new QueryResultDataset();
-        try {
-            SessionQueryDataSet sessionQueryDataSet = session.downsampleQuery(paths, tagList, startTimestamp, endTimestamp, aggregateType, getDur(), timePrecision);
-            queryResultDataset.setPaths(getPathsFromSessionQueryDataSet(sessionQueryDataSet));
-            DataType type = RestUtils.checkType(sessionQueryDataSet);
-            int n = sessionQueryDataSet.getKeys().length;
-            int m = sessionQueryDataSet.getPaths().size();
-            int datapoints = 0;
-            for (int j = 0; j < m; j++) {
-                List<Object> value = new ArrayList<>();
-                List<Long> time = new ArrayList<>();
-                for (int i = 0; i < n; i++) {
-                    if (sessionQueryDataSet.getValues().get(i).get(j) != null) {
-                        value.add(sessionQueryDataSet.getValues().get(i).get(j));
-                        long timeRes = sessionQueryDataSet.getKeys()[i];
-                        time.add(timeRes);
-                        queryResultDataset.add(timeRes, sessionQueryDataSet.getValues().get(i).get(j));
-                        datapoints += 1;
-                    }
-                }
-                queryResultDataset.addValueLists(value);
-                queryResultDataset.addTimeLists(time);
-            }
-            queryResultDataset.setSampleSize(datapoints);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         return queryResultDataset;
     }
 
