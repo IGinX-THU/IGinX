644826be316dc512e7471aa6032bf357ad65fd3d
jiang <jzl18thu@163.com>
2023-03-08 10:47:12 +0800

    Update UDFIT.java

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/udf/UDFIT.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/udf/UDFIT.java
@@ -18,10 +18,6 @@
  */
 package cn.edu.tsinghua.iginx.integration.udf;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
 import cn.edu.tsinghua.iginx.exceptions.ExecutionException;
 import cn.edu.tsinghua.iginx.exceptions.SessionException;
 import cn.edu.tsinghua.iginx.metadata.DefaultMetaManager;
@@ -30,9 +26,6 @@ import cn.edu.tsinghua.iginx.metadata.entity.TransformTaskMeta;
 import cn.edu.tsinghua.iginx.session.Session;
 import cn.edu.tsinghua.iginx.session.SessionExecuteSqlResult;
 import cn.edu.tsinghua.iginx.thrift.UDFType;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -41,6 +34,13 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class UDFIT {
 
@@ -152,7 +152,6 @@ public class UDFIT {
 
         SessionExecuteSqlResult ret = execute(statement);
         assertEquals(Collections.singletonList("cos(us.d1.s1)"), ret.getPaths());
-        assertArrayEquals(new long[]{0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L}, ret.getKeys());
 
         List<Double> expectedValues = Arrays
             .asList(1.0, 0.5403023058681398, -0.4161468365471424, -0.9899924966004454,
@@ -173,7 +172,7 @@ public class UDFIT {
 
         String query = "SELECT * FROM (SELECT COS(s1) FROM test), (SELECT COS(s2) FROM test) LIMIT 10;";
         SessionExecuteSqlResult ret = execute(query);
-        assertEquals(4, ret.getPaths().size());
+        assertEquals(2, ret.getPaths().size());
 
         List<Double> cosS1ExpectedValues = Arrays
             .asList(-0.4161468365471424, -0.4161468365471424, -0.4161468365471424,
@@ -185,7 +184,7 @@ public class UDFIT {
                 0.5403023058681398, -0.9899924966004454, 0.7539022543433046);
 
         for (int i = 0; i < ret.getValues().size(); i++) {
-            assertEquals(4, ret.getValues().get(i).size());
+            assertEquals(2, ret.getValues().get(i).size());
             double expected = cosS1ExpectedValues.get(i);
             double actual = (double) ret.getValues().get(i).get(0);
             assertEquals(expected, actual, delta);
