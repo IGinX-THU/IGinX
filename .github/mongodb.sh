336dbf3078b32fa91abfc28aad04e83b9876e0a9
Yuan Zi <ziy20@mails.tsinghua.edu.cn>
2023-03-10 09:21:23 +0800

    sh

--- .github/mongodb.sh
+++ .github/mongodb.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-6.0.4.tgz
+sh -c "wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-6.0.4.tgz"
+
+sh -c "tar -zxvf mongodb-linux-x86_64-ubuntu2204-6.0.4.tgz"
+
+sudo sh -c "cd imongodb-linux-x86_64-ubuntu2204-6.0.4/; nohup ./bin/mongod &"
