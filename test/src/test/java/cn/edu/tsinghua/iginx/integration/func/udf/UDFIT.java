911bcc47cc5163336bce723e7b066b52fafb3744
jzl18thu <jzl18thu@163.com>
2023-03-10 17:25:34 +0800

    dev select sub query (#37)
    * dev select sub query
    
    * add select sub query test & modify row transform
    
    * Update SQLSessionIT.java
    
    * Update UDFIT.java
    
    * fix a bug
    
    * add test
    
    1. add test mixing select-sub-query and from-sub-query
    2. the single join can return key now
    
    * Update SQLSessionIT.java
    
    add a test
    
    * Squashed commit of the following:
    
    commit 6855ef36d62ff69a7482685219e3e99881f10cbe
    Author: jzl18thu <jzl18thu@163.com>
    Date:   Fri Mar 10 10:50:05 2023 +0800
    
        bug fix & add level test (#36)
    
        * fix some bugs
    
        * fix & test
    
        1. fix a bug in project
        2. add level test
    
        * Update IginXSqlVisitor.java
    
        add some annotation
    
        * fix a bug in agg level
    
    * fix
    
    Change the execution order of select-sub-query and where filter
    
    * add single join unit test
    
    * Update SQLSessionIT.java
    
    add more arithmetic expression tests

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/func/udf/UDFIT.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/func/udf/UDFIT.java
@@ -18,10 +18,6 @@
  */
 package cn.edu.tsinghua.iginx.integration.func.udf;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
 import cn.edu.tsinghua.iginx.exceptions.ExecutionException;
 import cn.edu.tsinghua.iginx.exceptions.SessionException;
 import cn.edu.tsinghua.iginx.integration.controller.Controller;
@@ -31,9 +27,6 @@ import cn.edu.tsinghua.iginx.metadata.entity.TransformTaskMeta;
 import cn.edu.tsinghua.iginx.session.Session;
 import cn.edu.tsinghua.iginx.session.SessionExecuteSqlResult;
 import cn.edu.tsinghua.iginx.thrift.UDFType;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -42,6 +35,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class UDFIT {
 
