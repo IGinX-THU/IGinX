e039e5a32099596066db39b5f8519f55b7be56a8
zhanglingzhe <surevil@foxmail.com>
2023-03-14 10:23:28 +0800

    fix mongodb ci

--- .github/mongodb.sh
+++ .github/mongodb.sh
@@ -6,4 +6,4 @@ sh -c "wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-6.0
 
 sh -c "tar -zxvf mongodb-linux-x86_64-ubuntu2204-6.0.4.tgz"
 
-sudo sh -c "cd imongodb-linux-x86_64-ubuntu2204-6.0.4/; nohup ./bin/mongod &"
+sudo sh -c "cd mongodb-linux-x86_64-ubuntu2204-6.0.4/; nohup ./bin/mongod &"
--- .github/mongodb_macos.sh
+++ .github/mongodb_macos.sh
@@ -6,4 +6,4 @@ sh -c "wget https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-6.0.4.tgz"
 
 sh -c "tar -zxvf mongodb-macos-x86_64-6.0.4.tgz"
 
-sudo sh -c "cd imongodb-macos-x86_64-6.0.4/; nohup ./bin/mongod &"
+sudo sh -c "cd mongodb-macos-x86_64-6.0.4/; nohup ./bin/mongod &"
