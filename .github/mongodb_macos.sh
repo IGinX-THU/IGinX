9a8ba530d61db0913682a5216c98e501e83b9c48
zhanglingzhe <surevil@foxmail.com>
2023-03-14 14:10:18 +0800

    fix shell

--- .github/mongodb_macos.sh
+++ .github/mongodb_macos.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-sed -i "s/storageEngineList=127.0.0.1#6667/#storageEngineList=127.0.0.1#6667/g" conf/config.properties
+sed -i "" "s/storageEngineList=127.0.0.1#6667/#storageEngineList=127.0.0.1#6667/" conf/config.properties
 
-sed -i "s/#storageEngineList=127.0.0.1#27017/storageEngineList=127.0.0.1#27017/g" conf/config.properties
+sed -i "" "s/#storageEngineList=127.0.0.1#27017/storageEngineList=127.0.0.1#27017/" conf/config.properties
 
 sh -c "wget https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-6.0.4.tgz"
 
