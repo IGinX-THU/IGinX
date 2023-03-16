b6a605a7351c564be02083aacde066abb3ccb9a9
SolomonAnn <thss15_anyz@163.com>
2023-03-16 12:12:02 +0800

    Update macos startup shell

--- .github/postgresql_macos.sh
+++ .github/postgresql_macos.sh
@@ -8,6 +8,6 @@ sed -i "" "s/#storageEngineList=127.0.0.1#5432#postgresql/storageEngineList=127.
 
 sh -c "wget https://sbp.enterprisedb.com/getfile.jsp?fileid=1258319"
 
-sh -c "unzip postgresql-15.2-1-osx-binaries.zip"
+sh -c "sudo unzip postgresql-15.2-1-osx-binaries.zip"
 
 sh -c "cd pgsql; sudo mkdir -p /usr/local/var/postgresql@15; ./bin/initdb -D /usr/local/var/postgresql@15"
