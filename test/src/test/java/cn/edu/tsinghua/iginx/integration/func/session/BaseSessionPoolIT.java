3efa8885ccc49f1aa416ddbd944b6b221c49be24
RemHero <1104304963@qq.com>
2023-03-29 16:51:36 +0800

    add opensession() in BaseSessionPoolIT

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/func/session/BaseSessionPoolIT.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/func/session/BaseSessionPoolIT.java
@@ -5,21 +5,23 @@ import cn.edu.tsinghua.iginx.integration.tool.MultiConnection;
 import cn.edu.tsinghua.iginx.pool.SessionPool;
 import org.junit.Before;
 
-public class BaseSessionPoolIT extends BaseSessionIT{
+public class BaseSessionPoolIT extends BaseSessionIT {
 
     //check in BaseSessionIT for the initializations of MultiThreadTask before assignment
     private final int MaxMultiThreadTaskNum = 10;
+
     @Before
     @Override
     public void setUp() {
         try {
-            session = new MultiConnection( new SessionPool.Builder()
-                    .host(defaultTestHost)
-                    .port(defaultTestPort)
-                    .user(defaultTestUser)
-                    .password(defaultTestPass)
-                    .maxSize(MaxMultiThreadTaskNum)
-                    .build());
+            session = new MultiConnection(new SessionPool.Builder()
+                .host(defaultTestHost)
+                .port(defaultTestPort)
+                .user(defaultTestUser)
+                .password(defaultTestPass)
+                .maxSize(MaxMultiThreadTaskNum)
+                .build());
+            session.openSession();
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
