547165a9d92f75bd0396c231434a4b1e996d883d
SolomonAnn <thss15_anyz@163.com>
2023-05-19 00:42:17 +0800

    Fix history data generator

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/postgresql/PostgreSQLHistoryDataGeneratorTest.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/postgresql/PostgreSQLHistoryDataGeneratorTest.java
@@ -92,9 +92,9 @@ public class PostgreSQLHistoryDataGeneratorTest extends BaseHistoryDataGenerator
 
         stmt.execute(
                 String.format(
-                        CREATE_TABLE_STATEMENT, "wf01.wt01", "status boolean, temperature float8"));
+                        CREATE_TABLE_STATEMENT, "wf01\u2E82wt01", "status boolean, temperature float8"));
         stmt.execute(
-                String.format(INSERT_STATEMENT, "wf01.wt01", "(100, true, null), (200, false, 20.71)"));
+                String.format(INSERT_STATEMENT, "wf01\u2E82wt01", "(100, true, null), (200, false, 20.71)"));
 
         stmt.close();
         conn.close();
@@ -124,10 +124,10 @@ public class PostgreSQLHistoryDataGeneratorTest extends BaseHistoryDataGenerator
 
         stmt.execute(
                 String.format(
-                        CREATE_TABLE_STATEMENT, "wf03.wt01", "status boolean, temperature float8"));
+                        CREATE_TABLE_STATEMENT, "wf03\u2E82wt01", "status boolean, temperature float8"));
 
         stmt.execute(
-                String.format(INSERT_STATEMENT, "wf03.wt01", "(77, true, null), (200, false, 77.71)"));
+                String.format(INSERT_STATEMENT, "wf03\u2E82wt01", "(77, true, null), (200, false, 77.71)"));
 
         logger.info("write data to 127.0.0.1:5433 success!");
     }
