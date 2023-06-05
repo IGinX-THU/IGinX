7296934674af926464e16fc5dbb986d487876884
SolomonAnn <thss15_anyz@163.com>
2023-06-05 20:10:16 +0800

    Debug

--- .github/zk_clear.sh
+++ .github/zk_clear.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+sh -c "zookeeper/bin/zkCli.sh deleteall /iginx; zookeeper/bin/zkCli.sh deleteall /fragment; zookeeper/bin/zkCli.sh deleteall /lock; zookeeper/bin/zkCli.sh deleteall /storage; zookeeper/bin/zkCli.sh deleteall /unit; zookeeper/bin/zkCli.sh deleteall /statistics; zookeeper/bin/zkCli.sh deleteall /notification; zookeeper/bin/zkCli.sh deleteall /user; zookeeper/bin/zkCli.sh deleteall /counter; zookeeper/bin/zkCli.sh deleteall /schema; zookeeper/bin/zkCli.sh deleteall /policy; zookeeper/bin/zkCli.sh deleteall /timeseries; zookeeper/bin/zkCli.sh deleteall /transform"
+
 sh -c "zookeeper/bin/zkServer.sh stop"
 
 sh -c "sleep 2"
