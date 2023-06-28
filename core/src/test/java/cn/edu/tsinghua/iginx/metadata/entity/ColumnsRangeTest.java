80e17e2b5e8a3c464d1dc43a604e58a8d096a3ba
RemHero <1104304963@qq.com>
2023-06-28 09:39:01 +0800

    add the test for ColumnsRange

--- core/src/test/java/cn/edu/tsinghua/iginx/metadata/entity/ColumnsRangeTest.java
+++ core/src/test/java/cn/edu/tsinghua/iginx/metadata/entity/ColumnsRangeTest.java
@@ -2,12 +2,19 @@ package cn.edu.tsinghua.iginx.metadata.entity;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ColumnsRangeTest {
-
+  public static final Logger logger = LoggerFactory.getLogger(ColumnsRangeTest.class);
   private ColumnsRange columnsRange;
+  private String[] arrayName = {"null", "a.a", "b.b", "c.c", "d.d", "null"};
 
   @Before
   public void setUp() {
@@ -71,60 +78,6 @@ public class ColumnsRangeTest {
   }
 
   @Test
-  public void testIsContain() {
-    assertTrue(columnsRange.isContain("col1"));
-    assertTrue(columnsRange.isContain("col3"));
-    assertTrue(columnsRange.isContain("col5"));
-    assertFalse(columnsRange.isContain("col0"));
-    assertFalse(columnsRange.isContain("col6"));
-  }
-
-  @Test
-  public void testIsCompletelyBefore() {
-    assertFalse(columnsRange.isCompletelyBefore("col1"));
-    assertFalse(columnsRange.isCompletelyBefore("col3"));
-    assertTrue(columnsRange.isCompletelyBefore("col6"));
-  }
-
-  @Test
-  public void testIsIntersect() {
-    ColumnsRange other = new ColumnsRange("col3", "col7", true);
-    assertTrue(columnsRange.isIntersect(other));
-
-    other = new ColumnsRange("col6", "col7", false);
-    assertFalse(columnsRange.isIntersect(other));
-  }
-
-  @Test
-  public void testGetIntersect() {
-    ColumnsRange other = new ColumnsRange("col3", "col7", true);
-    ColumnsRange intersect = columnsRange.getIntersect(other);
-    assertNotNull(intersect);
-    assertEquals("col3", intersect.getStartColumn());
-    assertEquals("col5", intersect.getEndColumn());
-
-    other = new ColumnsRange("col6", "col7", false);
-    intersect = columnsRange.getIntersect(other);
-    assertNull(intersect);
-  }
-
-  @Test
-  public void testIsCompletelyAfter() {
-    ColumnsRange other = new ColumnsRange("col0");
-    assertTrue(columnsRange.isCompletelyAfter(other));
-
-    other = new ColumnsRange("col0", "col3", true);
-    assertFalse(columnsRange.isCompletelyAfter(other));
-  }
-
-  @Test
-  public void testIsAfter() {
-    assertTrue(columnsRange.isAfter("col0"));
-    assertFalse(columnsRange.isAfter("col1"));
-    assertFalse(columnsRange.isAfter("col3"));
-  }
-
-  @Test
   public void testCompareTo() {
     ColumnsRange other = new ColumnsRange("col1", "col5", true);
     assertEquals(0, columnsRange.compareTo(other));
@@ -137,19 +90,143 @@ public class ColumnsRangeTest {
   }
 
   @Test
-  public void testFromString() {
-    String input = "col1-col5";
-    ColumnsRange result = ColumnsRange.fromString(input);
-    assertNotNull(result);
-    assertEquals("col1", result.getStartColumn());
-    assertEquals("col5", result.getEndColumn());
-    assertFalse(result.isClosed());
+  public void testIsCompletelyAfter() {
+    List<Interval> intervals = getIntervals(Arrays.asList(0, 1, 2, 3, 4, 5));
+    for (int i = 0; i < intervals.size(); i++) {
+      for (int j = i; j < intervals.size(); j++) {
+        Interval intervalA = intervals.get(i);
+        Interval intervalB = intervals.get(j);
+        boolean res = testIsCompletelyAfter(intervalA, intervalB);
+        ColumnsRange ca = getColumnRangeFromInterval(intervalA);
+        ColumnsRange cb = getColumnRangeFromInterval(intervalB);
+        if (res != ca.isCompletelyAfter(cb)) {
+          logger.error(
+              "expect {} but was {}, with {}, {} ",
+              res,
+              ca.isCompletelyAfter(cb),
+              intervalA.toString(),
+              intervalB.toString());
+          fail();
+        }
+      }
+    }
+  }
 
-    input = "col1.*";
-    result = ColumnsRange.fromString(input);
-    assertNotNull(result);
-    assertEquals("col1.*", result.getStartColumn());
-    assertEquals("col1.*", result.getEndColumn());
-    assertTrue(result.isClosed());
+  @Test
+  public void testIsIntersect() {
+    List<Interval> intervals = getIntervals(Arrays.asList(0, 1, 2, 3, 4, 5));
+    for (int i = 0; i < intervals.size(); i++) {
+      for (int j = i; j < intervals.size(); j++) {
+        Interval intervalA = intervals.get(i);
+        Interval intervalB = intervals.get(j);
+        boolean res = testIsIntersect(intervalA, intervalB);
+        ColumnsRange ca = getColumnRangeFromInterval(intervalA);
+        ColumnsRange cb = getColumnRangeFromInterval(intervalB);
+        if (res != ca.isIntersect(cb)) {
+          logger.error(
+              "expect {} but was {}, with {}, {} ",
+              res,
+              ca.isIntersect(cb),
+              intervalA.toString(),
+              intervalB.toString());
+          fail();
+        }
+      }
+    }
+  }
+
+  @Test
+  public void testIsContain() {
+    List<Integer> list = Arrays.asList(0, 1, 2, 3, 4, 5);
+    List<Interval> intervals = getIntervals(list);
+
+    for (int i = 0; i < intervals.size(); i++) {
+      for (int j = 1; j < list.size() - 1; j++) {
+        Interval intervalA = intervals.get(i);
+        boolean res = testIsContain(intervalA, list.get(j));
+        ColumnsRange ca = getColumnRangeFromInterval(intervalA);
+        if (res != ca.isContain(arrayName[j])) {
+          logger.error(
+              "expect {} but was {}, with {}, {} ",
+              res,
+              ca.isContain(arrayName[j]),
+              intervalA.toString(),
+              arrayName[j]);
+          fail();
+        }
+      }
+    }
+  }
+
+  private ColumnsRange getColumnRangeFromInterval(Interval interval) {
+    int start = interval.start;
+    int end = interval.end;
+    boolean isClosed = interval.isClosed;
+    return new ColumnsRange(getNameFromIndex(start), getNameFromIndex(end), isClosed);
+  }
+
+  private String getNameFromIndex(int index) {
+    if (arrayName[index].equals("null")) {
+      return null;
+    } else {
+      return arrayName[index];
+    }
+  }
+
+  public List<Interval> getIntervals(List<Integer> numbers) {
+    List<Interval> intervals = new ArrayList<>();
+    for (int i = 0; i < numbers.size(); i++) {
+      for (int j = i; j < numbers.size(); j++) {
+        boolean isClosed = new Random().nextBoolean();
+        if (i == 0 && j == 0 || i == numbers.size() - 1 && j == numbers.size() - 1) continue;
+        intervals.add(new Interval(numbers.get(i), numbers.get(j), i == j ? true : isClosed));
+      }
+    }
+    return intervals;
+  }
+
+  private boolean testIsCompletelyAfter(Interval a, Interval b) {
+    if (b.isClosed) {
+      return a.start > b.end;
+    } else {
+      return a.start >= b.end;
+    }
+  }
+
+  public static boolean testIsIntersect(Interval a, Interval b) {
+    if (a.isClosed && b.isClosed) {
+      return !(a.end < b.start || a.start > b.end);
+    } else if (a.isClosed) {
+      return !(a.end < b.start || a.start >= b.end);
+    } else if (b.isClosed) {
+      return !(a.end <= b.start || a.start > b.end);
+    } else {
+      return !(a.end <= b.start || a.start >= b.end);
+    }
+  }
+
+  private boolean testIsContain(Interval a, int b) {
+    if (a.isClosed) {
+      return a.start <= b && a.end >= b;
+    } else {
+      return a.start <= b && a.end > b;
+    }
+  }
+
+  class Interval {
+    int start;
+    int end;
+    boolean isClosed;
+
+    public Interval(int start, int end, boolean isClosed) {
+      this.start = start;
+      this.end = end;
+      this.isClosed = isClosed;
+    }
+
+    @Override
+    public String toString() {
+      return "Interval [start=" + start + ", end=" + end + ", isClosed=" + isClosed + "]";
+    }
   }
 }
