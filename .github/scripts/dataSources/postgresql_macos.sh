b7202461b07823bafc1d1f23cb7aad433dd3947d
SolomonAnn <thss15_anyz@163.com>
2023-08-15 01:03:59 +0800

    Modify pg macos

--- .github/scripts/dataSources/postgresql_macos.sh
+++ .github/scripts/dataSources/postgresql_macos.sh
@@ -44,13 +44,11 @@ do
 
   sh -c "cd pgsql-$port/bin; sudo -u postgres ./initdb -D /var/lib/postgresql-$port/15/main --auth trust --no-instructions"
 
-  sh -c "sudo ls /var/lib/postgresql-$port/15/main"
-
-  sh -c "sudo cp /Users/runner/work/IGinX/IGinX/pgsql-$port/share/postgresql/postgresql.conf.sample /var/lib/postgresql-$port/15/main/postgresql.conf"
+  sh -c "sudo cp /var/lib/postgresql-$port/15/main/postgresql.conf.sample /var/lib/postgresql-$port/15/main/postgresql.conf"
 
   sh -c "sudo chmod -R 777 /var/lib/postgresql-$port/15/main/postgresql.conf"
 
-  sh -c "sudo echo \"max_identifier_length = 127\" >> /var/lib/postgresql-$port/15/main/postgresql.conf"
+  sh -c "echo \"max_identifier_length = 127\" | sudo tee -a /var/lib/postgresql-$port/15/main/postgresql.conf"
 
   sh -c "cd pgsql-$port/bin; sudo -u postgres ./pg_ctl -D /var/lib/postgresql-$port/15/main -o \"-F -p $port\" start"
 
