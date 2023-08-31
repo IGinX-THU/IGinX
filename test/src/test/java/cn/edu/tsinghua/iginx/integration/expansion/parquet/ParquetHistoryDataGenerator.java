60973a955f0876dc0e6a0e0c48bb6f33af645ed5
shinyano <12236590+shinyano@users.noreply.github.com>
2023-08-31 10:47:50 +0800

    fix dir column name

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/parquet/ParquetHistoryDataGenerator.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/parquet/ParquetHistoryDataGenerator.java
@@ -98,7 +98,7 @@ public class ParquetHistoryDataGenerator extends BaseHistoryDataGenerator {
       String dataType;
       for (int i = 0; i < columnCount; i++) {
         columnName = pathList.get(i).replace(IGINX_SEPARATOR, PARQUET_SEPARATOR);
-        columnName = columnName.substring(columnName.indexOf(PARQUET_SEPARATOR));
+        columnName = columnName.substring(columnName.indexOf(PARQUET_SEPARATOR) + 1);
         dataType = dataTypeList.get(i).toString();
 
         columnList.add(new Pair(columnName, dataType));
