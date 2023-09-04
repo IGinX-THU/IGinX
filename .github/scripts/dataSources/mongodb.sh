3d8294b15299e946de3e9c78a0129b61cac7a344
aqni <aqnin@outlook.com>
2023-09-04 10:46:59 +0800

    fix

--- .github/scripts/dataSources/mongodb.sh
+++ .github/scripts/dataSources/mongodb.sh
@@ -14,8 +14,6 @@ for port in "$@"
 do
   sudo sh -c "cp -r mongodb-linux-x86_64-ubuntu2204-6.0.4/ mongodb-linux-x86_64-ubuntu2204-6.0.4-$port/"
 
-  tree .
-
   sudo sh -c "cd mongodb-linux-x86_64-ubuntu2204-6.0.4-$port/; mkdir -p data/db; mkdir -p data/log; nohup ./bin/mongod --port $port --dbpath data/db --logpath data/log/mongo.log &"
 done
 
