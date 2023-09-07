f4f989e5272b97953dceade58520d002f76b0781
SolomonAnn <thss15_anyz@163.com>
2023-09-07 17:03:28 +0800

    Fix regex

--- dataSources/filesystem/src/main/java/cn/edu/tsinghua/iginx/filesystem/exec/FileSystemManager.java
+++ dataSources/filesystem/src/main/java/cn/edu/tsinghua/iginx/filesystem/exec/FileSystemManager.java
@@ -337,9 +337,9 @@ public class FileSystemManager {
           root = file.getParentFile();
         }
         if (isDummy) {
-          regex = filePath.replaceAll("[$^{}]", "\\\\$0").replaceAll("[*]", ".*");
+          regex = filePath.replaceAll("[$^{}\\\\]", "\\\\$0").replaceAll("[*]", ".*");
         } else {
-          regex = filePath.replaceAll("[$^{}]", "\\\\$0").replaceAll("[*]", ".*") + ".*";
+          regex = filePath.replaceAll("[$^{}\\\\]", "\\\\$0").replaceAll("[*]", ".*") + ".*";
         }
         Files.walkFileTree(
             root.toPath(),
