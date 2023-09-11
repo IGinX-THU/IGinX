412862d48b6f39fef192616455e8fc53e7a29082
ZM <12236590+shinyano@users.noreply.github.com>
2023-09-12 01:46:04 +0800

    Fix env export

--- .github/scripts/iginx/iginx_udf_path.sh
+++ .github/scripts/iginx/iginx_udf_path.sh
@@ -4,7 +4,7 @@ set -e
 
 sh -c "chmod +x core/target/iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.sh"
 
-export IGINX_HOME = $PWD
+export IGINX_HOME=$PWD
 
 # execute start-up script in different directory, to test whether udf-file path detection will be effected
 cd /core/target
