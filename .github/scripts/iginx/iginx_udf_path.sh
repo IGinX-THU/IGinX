a0cf64ed81c5513e85a65aa640aec350a3a14c9d
ZM <12236590+shinyano@users.noreply.github.com>
2023-09-12 01:51:31 +0800

    Fix iginx_udf_path.sh

--- .github/scripts/iginx/iginx_udf_path.sh
+++ .github/scripts/iginx/iginx_udf_path.sh
@@ -7,6 +7,6 @@ sh -c "chmod +x core/target/iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.sh"
 export IGINX_HOME=$PWD
 
 # execute start-up script in different directory, to test whether udf-file path detection will be effected
-cd /core/target
+cd core/target
 
 sh -c "nohup /iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.sh > iginx.log 2>&1 &"
