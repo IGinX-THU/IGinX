6ad86eb62a7101934c0cc853e26749966d9a9d69
shinyano <12236590+shinyano@users.noreply.github.com>
2023-09-12 08:23:35 +0800

    fix format

--- .github/scripts/iginx/iginx_udf_path.sh
+++ .github/scripts/iginx/iginx_udf_path.sh
@@ -9,4 +9,4 @@ export IGINX_HOME=$PWD
 # execute start-up script in different directory, to test whether udf-file path detection will be effected
 cd core/target
 
-sh -c "nohup /iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.sh > ../../iginx.log 2>&1 &"
+sh -c "nohup iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.sh > ../../iginx.log 2>&1 &"
