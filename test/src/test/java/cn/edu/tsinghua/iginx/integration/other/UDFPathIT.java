8f632f77cbd04c3d784d6c0f18d6229974da8158
shinyano <12236590+shinyano@users.noreply.github.com>
2023-09-14 21:37:04 +0800

    move udf path test to standalone-test.yml (test only standalone)

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/other/UDFPathIT.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/other/UDFPathIT.java
@@ -1,5 +1,6 @@
 package cn.edu.tsinghua.iginx.integration.other;
 
+import cn.edu.tsinghua.iginx.exceptions.ExecutionException;
 import cn.edu.tsinghua.iginx.exceptions.SessionException;
 import cn.edu.tsinghua.iginx.integration.expansion.utils.SQLTestTools;
 import cn.edu.tsinghua.iginx.session.Session;
@@ -8,6 +9,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class UDFPathIT {
 
@@ -21,6 +25,9 @@ public class UDFPathIT {
   private static String defaultTestUser = "root";
   private static String defaultTestPass = "root";
 
+  public static final List<String> UDF_LIST =
+          Arrays.asList("mn.wf01.wt01.status", "mn.wf01.wt01.temperature");
+
   @BeforeClass
   public static void setUp() throws SessionException {
     session = new Session(defaultTestHost, defaultTestPort, defaultTestUser, defaultTestPass);
@@ -33,7 +40,7 @@ public class UDFPathIT {
   }
 
   @Test
-  public void testUDFPath() {
+  public void testUDFFuncList() {
     String statement = "show register python task";
     String expectedRes =
         "Register task infos:\n"
@@ -49,6 +56,25 @@ public class UDFPathIT {
             + "|     udf_min|        UDFMin|          udf_min.py|0.0.0.0|    UDAF|\n"
             + "|     udf_avg|        UDFAvg|          udf_avg.py|0.0.0.0|    UDAF|\n"
             + "+------------+--------------+--------------------+-------+--------+\n";
+
+    SQLTestTools.executeAndCompare(session, statement, expectedRes);
+  }
+
+  // Test cos() to make sure udf scripts really exists.
+  @Test
+  public void testUDFFunction() throws SessionException, ExecutionException {
+    String insertStatement = "insert into aa.bb(key, value) values(1, 2)";
+    session.executeSql(insertStatement);
+
+    String statement = "select cos(value) from aa.bb";
+    String expectedRes =
+            "ResultSets:\n" +
+                    "+---+-------------------+\n" +
+                    "|key|   cos(aa.bb.value)|\n" +
+                    "+---+-------------------+\n" +
+                    "|  1|-0.4161468365471424|\n" +
+                    "+---+-------------------+\n" +
+                    "Total line number = 1";
 
     SQLTestTools.executeAndCompare(session, statement, expectedRes);
   }
