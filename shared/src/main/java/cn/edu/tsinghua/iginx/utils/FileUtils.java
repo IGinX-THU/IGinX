61c63c009ef01326e19b955be88a9cb3c2ad8d0a
jzl18thu <jzl18thu@163.com>
2023-09-21 17:52:49 +0800

    Update FileUtils.java
    使用 FileOutputStream

--- shared/src/main/java/cn/edu/tsinghua/iginx/utils/FileUtils.java
+++ shared/src/main/java/cn/edu/tsinghua/iginx/utils/FileUtils.java
@@ -1,10 +1,8 @@
 package cn.edu.tsinghua.iginx.utils;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -18,29 +16,25 @@ public class FileUtils {
       }
       try {
         File file = new File(columns[i]);
-        OutputStream os;
+        FileOutputStream fos;
         if (!file.exists()) {
           Files.createFile(Paths.get(file.getPath()));
-          os = Files.newOutputStream(file.toPath());
+          fos = new FileOutputStream(file);
         } else {
-          os = new FileOutputStream(file, true);
+          fos = new FileOutputStream(file, true);
         }
-        // 设置1M读写缓冲区
-        os = new BufferedOutputStream(os, 1024 * 1024);
 
         int finalI = i;
-        OutputStream finalOs = os;
         values.forEach(
             value -> {
               try {
-                finalOs.write(value.get(finalI));
+                fos.write(value.get(finalI));
               } catch (IOException e) {
                 throw new RuntimeException(e);
               }
             });
 
-        os.flush();
-        os.close();
+        fos.close();
       } catch (IOException e) {
         throw new RuntimeException(
             "Encounter an error when writing file " + columns[i] + ", because " + e.getMessage());
