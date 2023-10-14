9cc1520487df676d6535c4caf68e12587a5bc7ea
RemHero <1104304963@qq.com>
2023-10-14 14:31:56 +0800

    按要求修改 shell 文件

--- .github/scripts/dataSources/mix_iotdb12_influxdb_macos.sh
+++ .github/scripts/dataSources/mix_iotdb12_influxdb_macos.sh
@@ -10,6 +10,6 @@ sh -c ".github/scripts/dataSources/iotdb12_macos.sh 6667"
 
 sh -c ".github/scripts/dataSources/influxdb_macos.sh"
 
-set -i "s/storageEngineList/#storageEngineList/g" core/target/iginx-core-0.6.0-SNAPSHOT/conf/config.properties
+set -i "s/storageEngineList/#storageEngineList/g" conf/config.properties
 
-echo "storageEngineList=127.0.0.1#6667#iotdb12#username=root#password=root#sessionPoolSize=50#has_data=false#is_read_only=false,127.0.0.1#8086#influxdb#url=http://localhost:8086/#token=testToken#organization=testOrg#has_data=false" >> core/target/iginx-core-0.6.0-SNAPSHOT/conf/config.properties
+echo "storageEngineList=127.0.0.1#6667#iotdb12#username=root#password=root#sessionPoolSize=50#has_data=false#is_read_only=false,127.0.0.1#8086#influxdb#url=http://localhost:8086/#token=testToken#organization=testOrg#has_data=false" >> conf/config.properties
