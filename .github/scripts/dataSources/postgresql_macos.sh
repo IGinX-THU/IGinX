ba1c9b6ba9117e255a904a0809732c3ca7895f67
SolomonAnn <thss15_anyz@163.com>
2023-10-19 11:36:02 +0800

    Add password

--- .github/scripts/dataSources/postgresql_macos.sh
+++ .github/scripts/dataSources/postgresql_macos.sh
@@ -42,9 +42,9 @@ do
 
   sh -c "sudo chmod -R 777 /var/lib/postgresql-$port/15/main"
 
-  sh -c "cd pgsql-$port/bin; sudo su - postgres -c './initdb -D /var/lib/postgresql-$port/15/main --auth trust --no-instructions'"
+  sh -c "cd pgsql-$port/bin; echo "postgres" | sudo -u postgres -S ./initdb -D /var/lib/postgresql-$port/15/main --auth trust --no-instructions"
 
-  sh -c "cd pgsql-$port/bin; sudo su - postgres -c './pg_ctl -D /var/lib/postgresql-$port/15/main -o \"-F -p $port\" start'"
+  sh -c "cd pgsql-$port/bin; echo "postgres" | sudo -u postgres -S ./pg_ctl -D /var/lib/postgresql-$port/15/main -o \"-F -p $port\" start"
 
-  sh -c "cd pgsql-$port/bin; sudo su - postgres -c './psql -c \"ALTER USER postgres WITH PASSWORD '\''postgres'\'';\"'"
+  sh -c "cd pgsql-$port/bin; echo "postgres" | sudo -u postgres -S ./psql -c \"ALTER USER postgres WITH PASSWORD 'postgres';\""
 done
