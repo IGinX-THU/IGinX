f43fac40615633c92a10e6d36576c39408ec78d6
RemHero <1104304963@qq.com>
2023-10-20 10:30:04 +0800

    抽取版本号

--- .github/scripts/iginx/iginx.sh
+++ .github/scripts/iginx/iginx.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-sed -i "s/port=[0-9]\+/port=$1/g" core/target/iginx-core-*-SNAPSHOT/conf/config.properties
+sed -i "s/port=[0-9]\+/port=$1/g" core/target/iginx-core-*/conf/config.properties
 
-sed -i "s/#iginx_port=[0-9]\+#/#iginx_port=$1#/g" core/target/iginx-core-*-SNAPSHOT/conf/config.properties
+sed -i "s/#iginx_port=[0-9]\+#/#iginx_port=$1#/g" core/target/iginx-core-*/conf/config.properties
 
-sed -i "s/restPort=[0-9]\+/restPort=$2/g" core/target/iginx-core-*-SNAPSHOT/conf/config.properties
+sed -i "s/restPort=[0-9]\+/restPort=$2/g" core/target/iginx-core-*/conf/config.properties
 
-sh -c "chmod +x core/target/iginx-core-*-SNAPSHOT/sbin/start_iginx.sh"
+sh -c "chmod +x core/target/iginx-core-*/sbin/start_iginx.sh"
 
-sh -c "nohup core/target/iginx-core-*-SNAPSHOT/sbin/start_iginx.sh > iginx-$1.log 2>&1 &"
+sh -c "nohup core/target/iginx-core-*/sbin/start_iginx.sh > iginx-$1.log 2>&1 &"
