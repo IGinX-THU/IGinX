109db857543182513e8e0e1d10a53babd8266a27
shinyano <12236590+shinyano@users.noreply.github.com>
2023-10-24 16:02:43 +0800

    try to locate error

--- core/src/main/java/cn/edu/tsinghua/iginx/metadata/utils/StorageEngineUtils.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/metadata/utils/StorageEngineUtils.java
@@ -29,7 +29,7 @@ public class StorageEngineUtils {
     }
 
     // 检查路径是否为根路径
-    if (dir.equals("/") || dir.matches("[A-Za-z]:[/\\\\]")) {
+    if (dir.equals("/") || dir.matches("[A-Za-z]:(/|\\\\)")) {
       return false;
     }
 
