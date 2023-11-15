6ecbf2c980695fb1a9df8d1f772e0d00b72e3d7a
Jensen Wang <1092792221@qq.com>
2023-11-15 11:05:33 +0800

    format

--- core/src/test/java/cn/edu/tsinghua/iginx/optimizer/IteratorTest.java
+++ core/src/test/java/cn/edu/tsinghua/iginx/optimizer/IteratorTest.java
@@ -16,51 +16,71 @@ import org.junit.Test;
 
 public class IteratorTest {
 
-    @Test
-    public void testDeepFirstIterator() {
-        Operator root = TreeBuilder.buildSelectTree();
-        TreeIterator it = new DeepFirstIterator(root);
-        List<Class<? extends  Operator>> expectedOrder = Arrays.asList(Select.class, Project.class);
-        Assert.assertTrue(matchExpectedOrder(expectedOrder, it));
+  @Test
+  public void testDeepFirstIterator() {
+    Operator root = TreeBuilder.buildSelectTree();
+    TreeIterator it = new DeepFirstIterator(root);
+    List<Class<? extends Operator>> expectedOrder = Arrays.asList(Select.class, Project.class);
+    Assert.assertTrue(matchExpectedOrder(expectedOrder, it));
 
-        root = TreeBuilder.buildJoinTree0();
-        it = new DeepFirstIterator(root);
-        expectedOrder = Arrays.asList(Reorder.class, Select.class, InnerJoin.class, Project.class, Project.class);
-        Assert.assertTrue(matchExpectedOrder(expectedOrder, it));
+    root = TreeBuilder.buildJoinTree0();
+    it = new DeepFirstIterator(root);
+    expectedOrder =
+        Arrays.asList(Reorder.class, Select.class, InnerJoin.class, Project.class, Project.class);
+    Assert.assertTrue(matchExpectedOrder(expectedOrder, it));
 
-        root = TreeBuilder.buildJoinTree1();
-        it = new DeepFirstIterator(root);
-        expectedOrder = Arrays.asList(Reorder.class, Select.class, OuterJoin.class, InnerJoin.class, Project.class, Select.class, Project.class, Project.class);
-        Assert.assertTrue(matchExpectedOrder(expectedOrder, it));
+    root = TreeBuilder.buildJoinTree1();
+    it = new DeepFirstIterator(root);
+    expectedOrder =
+        Arrays.asList(
+            Reorder.class,
+            Select.class,
+            OuterJoin.class,
+            InnerJoin.class,
+            Project.class,
+            Select.class,
+            Project.class,
+            Project.class);
+    Assert.assertTrue(matchExpectedOrder(expectedOrder, it));
+  }
+
+  @Test
+  public void testLeveledIterator() {
+    Operator root = TreeBuilder.buildSelectTree();
+    TreeIterator it = new LeveledIterator(root);
+    List<Class<? extends Operator>> expectedOrder = Arrays.asList(Select.class, Project.class);
+    Assert.assertTrue(matchExpectedOrder(expectedOrder, it));
+
+    root = TreeBuilder.buildJoinTree0();
+    it = new LeveledIterator(root);
+    expectedOrder =
+        Arrays.asList(Reorder.class, Select.class, InnerJoin.class, Project.class, Project.class);
+    Assert.assertTrue(matchExpectedOrder(expectedOrder, it));
+
+    root = TreeBuilder.buildJoinTree1();
+    it = new LeveledIterator(root);
+    expectedOrder =
+        Arrays.asList(
+            Reorder.class,
+            Select.class,
+            OuterJoin.class,
+            InnerJoin.class,
+            Project.class,
+            Project.class,
+            Select.class,
+            Project.class);
+    Assert.assertTrue(matchExpectedOrder(expectedOrder, it));
+  }
+
+  private boolean matchExpectedOrder(List<Class<? extends Operator>> opList, TreeIterator it) {
+    int index = 0;
+    while (index < opList.size() && it.hasNext()) {
+      Operator op = it.next();
+      if (!opList.get(index).isInstance(op)) {
+        return false;
+      }
+      index++;
     }
-
-    @Test
-    public void testLeveledIterator() {
-        Operator root = TreeBuilder.buildSelectTree();
-        TreeIterator it = new LeveledIterator(root);
-        List<Class<? extends  Operator>> expectedOrder = Arrays.asList(Select.class, Project.class);
-        Assert.assertTrue(matchExpectedOrder(expectedOrder, it));
-
-        root = TreeBuilder.buildJoinTree0();
-        it = new LeveledIterator(root);
-        expectedOrder = Arrays.asList(Reorder.class, Select.class, InnerJoin.class, Project.class, Project.class);
-        Assert.assertTrue(matchExpectedOrder(expectedOrder, it));
-
-        root = TreeBuilder.buildJoinTree1();
-        it = new LeveledIterator(root);
-        expectedOrder = Arrays.asList(Reorder.class, Select.class, OuterJoin.class, InnerJoin.class, Project.class, Project.class, Select.class, Project.class);
-        Assert.assertTrue(matchExpectedOrder(expectedOrder, it));
-    }
-
-    private boolean matchExpectedOrder(List<Class<? extends  Operator>> opList, TreeIterator it) {
-        int index = 0;
-        while (index < opList.size() && it.hasNext()) {
-            Operator op = it.next();
-            if (!opList.get(index).isInstance(op)) {
-                return false;
-            }
-            index++;
-        }
-        return index >= opList.size() && !it.hasNext();
-    }
+    return index >= opList.size() && !it.hasNext();
+  }
 }
--- core/src/test/java/cn/edu/tsinghua/iginx/optimizer/TreeBuilder.java
+++ core/src/test/java/cn/edu/tsinghua/iginx/optimizer/TreeBuilder.java
@@ -16,56 +16,50 @@ import java.util.Collections;
 
 public class TreeBuilder {
 
-    /**
-     * select | project
-     */
-    public static Operator buildSelectTree() {
-        Project project = new Project(EmptySource.EMPTY_SOURCE, Collections.emptyList(), null);
-        return new Select(new OperatorSource(project), new KeyFilter(Op.G, 10), null);
-    }
+  /** select | project */
+  public static Operator buildSelectTree() {
+    Project project = new Project(EmptySource.EMPTY_SOURCE, Collections.emptyList(), null);
+    return new Select(new OperatorSource(project), new KeyFilter(Op.G, 10), null);
+  }
 
-    /**
-     * reorder | select | innerJoin /     \ projectA  projectB
-     */
-    public static Operator buildJoinTree0() {
-        Project projectA = new Project(EmptySource.EMPTY_SOURCE, Collections.emptyList(), null);
-        Project projectB = new Project(EmptySource.EMPTY_SOURCE, Collections.emptyList(), null);
+  /** reorder | select | innerJoin / \ projectA projectB */
+  public static Operator buildJoinTree0() {
+    Project projectA = new Project(EmptySource.EMPTY_SOURCE, Collections.emptyList(), null);
+    Project projectB = new Project(EmptySource.EMPTY_SOURCE, Collections.emptyList(), null);
 
-        InnerJoin innerJoin =
-            new InnerJoin(
-                new OperatorSource(projectA),
-                new OperatorSource(projectB),
-                null,
-                null,
-                new PathFilter("test.a", Op.E, "test.b"),
-                null);
+    InnerJoin innerJoin =
+        new InnerJoin(
+            new OperatorSource(projectA),
+            new OperatorSource(projectB),
+            null,
+            null,
+            new PathFilter("test.a", Op.E, "test.b"),
+            null);
 
-        Select select = new Select(new OperatorSource(innerJoin), new KeyFilter(Op.G, 10), null);
+    Select select = new Select(new OperatorSource(innerJoin), new KeyFilter(Op.G, 10), null);
 
-        return new Reorder(new OperatorSource(select), Collections.singletonList("*"));
-    }
+    return new Reorder(new OperatorSource(select), Collections.singletonList("*"));
+  }
 
-    /**
-     * reorder | selectB | outerJoin /     \ innerJoin  projectC /     \ projectA  selectA |
-     * projectB
-     */
-    public static Operator buildJoinTree1() {
-        Project projectA = new Project(EmptySource.EMPTY_SOURCE, Collections.emptyList(), null);
-        Project projectB = new Project(EmptySource.EMPTY_SOURCE, Collections.emptyList(), null);
+  /** reorder | selectB | outerJoin / \ innerJoin projectC / \ projectA selectA | projectB */
+  public static Operator buildJoinTree1() {
+    Project projectA = new Project(EmptySource.EMPTY_SOURCE, Collections.emptyList(), null);
+    Project projectB = new Project(EmptySource.EMPTY_SOURCE, Collections.emptyList(), null);
 
-        Select selectA = new Select(new OperatorSource(projectB), new KeyFilter(Op.G, 100), null);
+    Select selectA = new Select(new OperatorSource(projectB), new KeyFilter(Op.G, 100), null);
 
-        InnerJoin innerJoin =
-            new InnerJoin(
-                new OperatorSource(projectA),
-                new OperatorSource(selectA),
-                null,
-                null,
-                new PathFilter("test.a", Op.E, "test.b"),
-                null);
+    InnerJoin innerJoin =
+        new InnerJoin(
+            new OperatorSource(projectA),
+            new OperatorSource(selectA),
+            null,
+            null,
+            new PathFilter("test.a", Op.E, "test.b"),
+            null);
 
-        Project projectC = new Project(EmptySource.EMPTY_SOURCE, Collections.emptyList(), null);
-        OuterJoin outerJoin = new OuterJoin(
+    Project projectC = new Project(EmptySource.EMPTY_SOURCE, Collections.emptyList(), null);
+    OuterJoin outerJoin =
+        new OuterJoin(
             new OperatorSource(innerJoin),
             new OperatorSource(projectC),
             null,
@@ -74,8 +68,8 @@ public class TreeBuilder {
             new PathFilter("test.c", Op.E, "test.d"),
             null);
 
-        Select selectB = new Select(new OperatorSource(outerJoin), new KeyFilter(Op.G, 10), null);
+    Select selectB = new Select(new OperatorSource(outerJoin), new KeyFilter(Op.G, 10), null);
 
-        return new Reorder(new OperatorSource(selectB), Collections.singletonList("*"));
-    }
+    return new Reorder(new OperatorSource(selectB), Collections.singletonList("*"));
+  }
 }
