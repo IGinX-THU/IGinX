16887012eaeb5a13e2e6d6de404dc0e192774ab8
shinyano <12236590+shinyano@users.noreply.github.com>
2023-11-24 10:22:59 +0800

    test action on windows

---
+++ .github/scripts/metadata/zk_windows.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+sh -c "wget -nv https://dlcdn.apache.org/zookeeper/zookeeper-3.7.2/apache-zookeeper-3.7.2-bin.tar.gz --no-check-certificate"
+
+sh -c "tar -zxf apache-zookeeper-3.7.2-bin.tar.gz"
+
+sh -c "mv apache-zookeeper-3.7.2-bin zookeeper"
+
+sh -c "cp ./.github/actions/zookeeperRunner/zooWin.cfg zookeeper/conf/zoo.cfg"
+
+sh -c "zookeeper/bin/zkServer.sh start"
