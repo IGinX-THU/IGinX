1a52926318f994818ead2ee2acec19161f416cd0
An Qi <aqnin@outlook.com>
2024-01-02 10:13:08 +0800

    fix

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/parquet/ParquetHistoryDataGenerator.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/parquet/ParquetHistoryDataGenerator.java
@@ -102,7 +102,7 @@ public class ParquetHistoryDataGenerator extends BaseHistoryDataGenerator {
       StringBuilder typeListStr = new StringBuilder();
       StringBuilder insertStr;
       for (Pair<String, String> p : columnList) {
-        typeListStr.append(p.k).append(" ").append(p.v).append(", ");
+        typeListStr.append("\"").append(p.k).append("\" ").append(p.v).append(", ");
       }
 
       stmt.execute(
