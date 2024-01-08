6af7f20ab783083993629a11b7343043653cb2bc
shinyano <12236590+shinyano@users.noreply.github.com>
2024-01-08 20:31:13 +0800

    show iginx log

--- .github/scripts/test/cli/test_outfile.sh
+++ .github/scripts/test/cli/test_outfile.sh
@@ -10,11 +10,11 @@ COMMAND+='insert into test(key, s2) values (0, 0.5), (1, 1.5), (2, 2.5), (3, 3.5
 
 COMMAND+='insert into test(key, s3) values (0, true), (1, false), (2, true), (3, false), (4, true);'
 
-COMMAND+='insert into test(key, s4) values (0, "'"aaa"'"), (1, \"bbb\"), (2, \"ccc\"), (3, \"ddd\"), (4, \"eee\");'
+COMMAND+='insert into test(key, s4) values (0, "'"aaa"'"), (1, "'"bbb"'"), (2, "'"ccc"'"), (3, "'"ddd"'"), (4, "'"eee"'");'
 
-COMMAND+='select * from test into outfile \"test/src/test/resources/fileReadAndWrite/byteStream\" as stream;'
+COMMAND+='select * from test into outfile "'"test/src/test/resources/fileReadAndWrite/byteStream"'" as stream;'
 
-COMMAND+='select * from test into outfile \"test/src/test/resources/fileReadAndWrite/csv/test.csv\" as csv;'
+COMMAND+='select * from test into outfile "'"test/src/test/resources/fileReadAndWrite/csv/test.csv"'" as csv;'
 
 bash -c "chmod +x client/target/iginx-client-0.6.0-SNAPSHOT/sbin/start_cli.sh"
 
@@ -39,6 +39,6 @@ bash -c "wget -nv $downloadURL --directory-prefix=downloads"
 # 将 downloads/large_img.jpg 的数据加载到IGinX数据库中
 bash -c "mvn test -q -Dtest=FileLoaderTest#loadLargeImage -DfailIfNoTests=false -P-format"
 
-OUTFILE_COMMAND='select large_img_jpg from downloads into outfile \"test/src/test/resources/fileReadAndWrite/img_outfile\" as stream;'
+OUTFILE_COMMAND='select large_img_jpg from downloads into outfile "'"test/src/test/resources/fileReadAndWrite/img_outfile"'" as stream;'
 
 bash -c "echo '$OUTFILE_COMMAND' | xargs -0 -t -i ${SCRIPT_COMMAND}"
