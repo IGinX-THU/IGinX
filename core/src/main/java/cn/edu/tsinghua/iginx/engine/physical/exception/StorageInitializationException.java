0cd3883a96076ab87a5263f31b87406b6c9802bb
An Qi <aqnin@outlook.com>
2024-01-10 19:31:48 +0800

    better postgreSQL

--- core/src/main/java/cn/edu/tsinghua/iginx/engine/physical/exception/StorageInitializationException.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/engine/physical/exception/StorageInitializationException.java
@@ -25,8 +25,4 @@ public class StorageInitializationException extends PhysicalException {
   public StorageInitializationException(String message) {
     super(message);
   }
-
-  public StorageInitializationException(String message, Throwable cause) {
-    super(message, cause);
-  }
 }
