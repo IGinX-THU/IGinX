b0c469fcd5b42311e5d02bd407910eb49002604c
aqni <aqnin@outlook.com>
2024-02-01 15:01:03 +0800

    fix conflicts of ParquetHistoryDataGenerator

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/parquet/ParquetHistoryDataGenerator.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/parquet/ParquetHistoryDataGenerator.java
@@ -8,37 +8,45 @@ import cn.edu.tsinghua.iginx.parquet.manager.dummy.Table;
 import cn.edu.tsinghua.iginx.thrift.DataType;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ParquetHistoryDataGenerator extends BaseHistoryDataGenerator {
 
-  private static final Logger logger = LoggerFactory.getLogger(ParquetHistoryDataGenerator.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(ParquetHistoryDataGenerator.class);
 
   private static final char IGINX_SEPARATOR = '.';
 
+  public static final String IT_DATA_DIR = "IT_data";
+
+  public static final String IT_DATA_FILENAME = "data.parquet";
+
   public ParquetHistoryDataGenerator() {}
 
-  @Override
   public void writeHistoryData(
-      int port, List<String> pathList, List<DataType> dataTypeList, List<List<Object>> valuesList) {
+      int port,
+      String dir,
+      String filename,
+      List<String> pathList,
+      List<DataType> dataTypeList,
+      List<Long> keyList,
+      List<List<Object>> valuesList) {
     if (!PARQUET_PARAMS.containsKey(port)) {
-      logger.error("writing to unknown port {}.", port);
+      LOGGER.error("writing to unknown port {}.", port);
       return;
     }
 
-    String dir = "test" + System.getProperty("file.separator") + PARQUET_PARAMS.get(port).get(0);
-    String filename = PARQUET_PARAMS.get(port).get(1);
     Path dirPath = Paths.get("../" + dir);
     if (Files.notExists(dirPath)) {
       try {
         Files.createDirectories(dirPath);
       } catch (IOException e) {
-        logger.error("can't create data file path {}.", dir);
+        LOGGER.error("can't create data file path {}.", dir);
         return;
       }
     }
@@ -52,8 +60,9 @@ public class ParquetHistoryDataGenerator extends BaseHistoryDataGenerator {
       table.declareColumn(columnName, dataTypeList.get(fieldIndex));
     }
 
-    for (int key = 0; key < valuesList.size(); key++) {
-      List<Object> rowData = valuesList.get(key);
+    for (int i = 0; i < valuesList.size(); i++) {
+      long key = keyList.get(i);
+      List<Object> rowData = valuesList.get(i);
       for (int fieldIndex = 0; fieldIndex < pathList.size(); fieldIndex++) {
         table.put(fieldIndex, key, rowData.get(fieldIndex));
       }
@@ -68,9 +77,29 @@ public class ParquetHistoryDataGenerator extends BaseHistoryDataGenerator {
   }
 
   @Override
+  public void writeHistoryData(
+      int port,
+      List<String> pathList,
+      List<DataType> dataTypeList,
+      List<Long> keyList,
+      List<List<Object>> valuesList) {
+    String dir = "test" + System.getProperty("file.separator") + PARQUET_PARAMS.get(port).get(0);
+    String filename = PARQUET_PARAMS.get(port).get(1);
+    writeHistoryData(port, dir, filename, pathList, dataTypeList, keyList, valuesList);
+  }
+
+  @Override
+  public void writeHistoryData(
+      int port, List<String> pathList, List<DataType> dataTypeList, List<List<Object>> valuesList) {
+    List<Long> keyList =
+        Stream.iterate(0L, n -> n + 1).limit(valuesList.size()).collect(Collectors.toList());
+    writeHistoryData(port, pathList, dataTypeList, keyList, valuesList);
+  }
+
+  @Override
   public void clearHistoryDataForGivenPort(int port) {
     if (!PARQUET_PARAMS.containsKey(port)) {
-      logger.error("delete from unknown port {}.", port);
+      LOGGER.error("delete from unknown port {}.", port);
       return;
     }
 
@@ -82,7 +111,31 @@ public class ParquetHistoryDataGenerator extends BaseHistoryDataGenerator {
     if (file.exists() && file.isFile()) {
       file.delete();
     } else {
-      logger.error("delete {}/{} error: does not exist or is not a file.", dir, filename);
+      LOGGER.error("delete {}/{} error: does not exist or is not a file.", dir, filename);
+    }
+
+    // delete the normal IT data
+    dir = IT_DATA_DIR + System.getProperty("file.separator");
+    parquetPath = Paths.get("../" + dir);
+
+    try {
+      Files.walkFileTree(parquetPath, new DeleteFileVisitor());
+    } catch (IOException e) {
+      LOGGER.warn("delete {} error", dir, e);
+    }
+  }
+
+  static class DeleteFileVisitor extends SimpleFileVisitor<Path> {
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+      Files.delete(file);
+      return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+      Files.delete(dir);
+      return FileVisitResult.CONTINUE;
     }
   }
 }
