a8bbf0d1c022736c3bb172bbe3dfdf74b6663626
shinyano <12236590+shinyano@users.noreply.github.com>
2024-03-21 23:52:31 +0800

    fix bug

--- core/src/main/java/cn/edu/tsinghua/iginx/policy/historical/HistoricalPolicy.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/policy/historical/HistoricalPolicy.java
@@ -155,87 +155,87 @@ public class HistoricalPolicy extends AbstractPolicy implements IPolicy {
     return new Pair<>(fragmentList, storageUnitList);
   }
 
-//  @Override
-//  public Pair<List<FragmentMeta>, List<StorageUnitMeta>> generateFragmentsAndStorageUnits(
-//      DataStatement statement) {
-//    long startKey;
-//    if (statement.getType() == StatementType.INSERT) {
-//      startKey =
-//          ((InsertStatement) statement).getEndKey()
-//              + TimeUnit.SECONDS.toMillis(
-//                      ConfigDescriptor.getInstance().getConfig().getDisorderMargin())
-//                  * 2
-//              + 1;
-//    } else {
-//      throw new IllegalArgumentException(
-//          "function generateFragmentsAndStorageUnits only use insert statement for now.");
-//    }
-//
-//    List<FragmentMeta> fragmentList = new ArrayList<>();
-//    List<StorageUnitMeta> storageUnitList = new ArrayList<>();
-//    int storageEngineNum = iMetaManager.getWritableStorageEngineList().size();
-//    int replicaNum =
-//        Math.min(1 + ConfigDescriptor.getInstance().getConfig().getReplicaNum(), storageEngineNum);
-//    List<Long> storageEngineIdList;
-//    Pair<FragmentMeta, StorageUnitMeta> pair;
-//
-//    if (storageEngineNum == 0) {
-//      // 系统中无可写存储引擎
-//      throw new IllegalArgumentException("there are no writable storage engines!");
-//    } else if (storageEngineNum == 1) {
-//      // 系统中只有一个可写存储引擎
-//      // [startKey, +∞) & (null, null)
-//      storageEngineIdList =
-//          Collections.singletonList(iMetaManager.getWritableStorageEngineList().get(0).getId());
-//      pair =
-//          generateFragmentAndStorageUnitByColumnsIntervalAndKeyInterval(
-//              null, null, startKey, Long.MAX_VALUE, storageEngineIdList);
-//      fragmentList.add(pair.k);
-//      storageUnitList.add(pair.v);
-//    } else {
-//      // 系统中有多个可写存储引擎
-//      List<String> prefixList = sampler.samplePrefix(storageEngineNum - 1);
-//
-//      int index = 0;
-//
-//      // [startKey, +∞) & [startPath, endPath)
-//      int splitNum = Math.max(Math.min(storageEngineNum, prefixList.size() - 1), 0);
-//      for (int i = 0; i < splitNum; i++) {
-//        storageEngineIdList = generateStorageEngineIdList(index++, replicaNum);
-//        pair =
-//            generateFragmentAndStorageUnitByColumnsIntervalAndKeyInterval(
-//                prefixList.get(i),
-//                prefixList.get(i + 1),
-//                startKey,
-//                Long.MAX_VALUE,
-//                storageEngineIdList);
-//        fragmentList.add(pair.k);
-//        storageUnitList.add(pair.v);
-//      }
-//
-//      // [startKey, +∞) & [endPath, null)
-//      storageEngineIdList = generateStorageEngineIdList(index++, replicaNum);
-//      pair =
-//          generateFragmentAndStorageUnitByColumnsIntervalAndKeyInterval(
-//              prefixList.get(prefixList.size() - 1),
-//              null,
-//              startKey,
-//              Long.MAX_VALUE,
-//              storageEngineIdList);
-//      fragmentList.add(pair.k);
-//      storageUnitList.add(pair.v);
-//
-//      // [startKey, +∞) & (null, startPath)
-//      storageEngineIdList = generateStorageEngineIdList(index, replicaNum);
-//      pair =
-//          generateFragmentAndStorageUnitByColumnsIntervalAndKeyInterval(
-//              null, prefixList.get(0), startKey, Long.MAX_VALUE, storageEngineIdList);
-//      fragmentList.add(pair.k);
-//      storageUnitList.add(pair.v);
-//    }
-//
-//    return new Pair<>(fragmentList, storageUnitList);
-//  }
+  @Override
+  public Pair<List<FragmentMeta>, List<StorageUnitMeta>> generateFragmentsAndStorageUnits(
+      DataStatement statement) {
+    long startKey;
+    if (statement.getType() == StatementType.INSERT) {
+      startKey =
+          ((InsertStatement) statement).getEndKey()
+              + TimeUnit.SECONDS.toMillis(
+                      ConfigDescriptor.getInstance().getConfig().getDisorderMargin())
+                  * 2
+              + 1;
+    } else {
+      throw new IllegalArgumentException(
+          "function generateFragmentsAndStorageUnits only use insert statement for now.");
+    }
+
+    List<FragmentMeta> fragmentList = new ArrayList<>();
+    List<StorageUnitMeta> storageUnitList = new ArrayList<>();
+    int storageEngineNum = iMetaManager.getWritableStorageEngineList().size();
+    int replicaNum =
+        Math.min(1 + ConfigDescriptor.getInstance().getConfig().getReplicaNum(), storageEngineNum);
+    List<Long> storageEngineIdList;
+    Pair<FragmentMeta, StorageUnitMeta> pair;
+
+    if (storageEngineNum == 0) {
+      // 系统中无可写存储引擎
+      throw new IllegalArgumentException("there are no writable storage engines!");
+    } else if (storageEngineNum == 1) {
+      // 系统中只有一个可写存储引擎
+      // [startKey, +∞) & (null, null)
+      storageEngineIdList =
+          Collections.singletonList(iMetaManager.getWritableStorageEngineList().get(0).getId());
+      pair =
+          generateFragmentAndStorageUnitByColumnsIntervalAndKeyInterval(
+              null, null, startKey, Long.MAX_VALUE, storageEngineIdList);
+      fragmentList.add(pair.k);
+      storageUnitList.add(pair.v);
+    } else {
+      // 系统中有多个可写存储引擎
+      List<String> prefixList = sampler.samplePrefix(storageEngineNum - 1);
+
+      int index = 0;
+
+      // [startKey, +∞) & [startPath, endPath)
+      int splitNum = Math.max(Math.min(storageEngineNum, prefixList.size() - 1), 0);
+      for (int i = 0; i < splitNum; i++) {
+        storageEngineIdList = generateStorageEngineIdList(index++, replicaNum);
+        pair =
+            generateFragmentAndStorageUnitByColumnsIntervalAndKeyInterval(
+                prefixList.get(i),
+                prefixList.get(i + 1),
+                startKey,
+                Long.MAX_VALUE,
+                storageEngineIdList);
+        fragmentList.add(pair.k);
+        storageUnitList.add(pair.v);
+      }
+
+      // [startKey, +∞) & [endPath, null)
+      storageEngineIdList = generateStorageEngineIdList(index++, replicaNum);
+      pair =
+          generateFragmentAndStorageUnitByColumnsIntervalAndKeyInterval(
+              prefixList.get(prefixList.size() - 1),
+              null,
+              startKey,
+              Long.MAX_VALUE,
+              storageEngineIdList);
+      fragmentList.add(pair.k);
+      storageUnitList.add(pair.v);
+
+      // [startKey, +∞) & (null, startPath)
+      storageEngineIdList = generateStorageEngineIdList(index, replicaNum);
+      pair =
+          generateFragmentAndStorageUnitByColumnsIntervalAndKeyInterval(
+              null, prefixList.get(0), startKey, Long.MAX_VALUE, storageEngineIdList);
+      fragmentList.add(pair.k);
+      storageUnitList.add(pair.v);
+    }
+
+    return new Pair<>(fragmentList, storageUnitList);
+  }
 //
 //  private List<Long> generateStorageEngineIdList(int startIndex, int num) {
 //    List<Long> storageEngineIdList = new ArrayList<>();
