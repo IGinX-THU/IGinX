7c5e4ec9f8f9ccc0fc51fa35433df56369ae3293
shinyano <12236590+shinyano@users.noreply.github.com>
2024-03-25 10:13:27 +0800

    revert column names to normal file path in 'into outfile as stream' function

--- shared/src/main/java/cn/edu/tsinghua/iginx/utils/FileUtils.java
+++ shared/src/main/java/cn/edu/tsinghua/iginx/utils/FileUtils.java
@@ -18,6 +18,13 @@ public class FileUtils {
         File file = new File(columns[i]);
         FileOutputStream fos;
         if (!file.exists()) {
+          String parentPath = file.getParent();
+          if (parentPath != null) {
+            // recursively create dir
+            if (!createDirectory(parentPath)) {
+              throw new RuntimeException("can't create file: " + columns[i]);
+            }
+          }
           Files.createFile(Paths.get(file.getPath()));
           fos = new FileOutputStream(file);
         } else {
@@ -40,5 +47,35 @@ public class FileUtils {
             "Encounter an error when writing file " + columns[i] + ", because " + e.getMessage());
       }
     }
+  }
+
+  // swap two char in string
+  public static String swapChar(String str, char a, char b) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i<str.length(); i++) {
+      if (str.charAt(i) == a) {
+        sb.append(b);
+      } else if (str.charAt(i) == b) {
+        sb.append(a);
+      } else {
+        sb.append(str.charAt(i));
+      }
+    }
+    return sb.toString();
+  }
+
+  // create dir recursively
+  private static boolean createDirectory(String directoryPath) {
+    File directory = new File(directoryPath);
+    if (directory.exists()) {
+      return true;
+    }
+    String parentPath = directory.getParent();
+    if (parentPath != null) {
+      if (!createDirectory(parentPath)) {
+        return false;
+      }
+    }
+    return directory.mkdir();
   }
 }
