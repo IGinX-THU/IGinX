3f7735d1ec00a28e3646c1e05d88da924c642c3b
shinyano <12236590+shinyano@users.noreply.github.com>
2024-03-29 15:24:22 +0800

    merge duplicated code(commented)

--- shared/src/test/java/cn/edu/tsinghua/iginx/utils/StringUtilsTest.java
+++ shared/src/test/java/cn/edu/tsinghua/iginx/utils/StringUtilsTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class StringUtilsTest {
 
   @Before
@@ -15,5 +19,42 @@ public class StringUtilsTest {
     String str = ".^${}+?()[]|\\\\*";
     String result = StringUtils.reformatPath(str);
     assertEquals(result, "\\.\\^\\$\\{\\}\\+\\?\\(\\)\\[\\]\\|\\\\\\\\.*");
+  }
+
+  @Test
+  public void t1() {
+    String path = "test.a{s=2,a=6}";
+    StringBuilder name = new StringBuilder("test.a");
+    Map<String, String> ret = new LinkedHashMap<>();
+    fill(path, name, ret);
+    System.out.println(ret);
+  }
+
+  @Test
+  public void t2() {
+    String path = "test.a{s=2,a=6}";
+    StringBuilder name = new StringBuilder("test.a");
+    Map<String, String> ret = new TreeMap<>();
+    fill(path, name, ret);
+    System.out.println(ret);
+  }
+
+  public void fill(String path, StringBuilder name, Map<String, String> ret) {
+
+    int firstBrace = path.indexOf("{");
+    int lastBrace = path.indexOf("}");
+    if (firstBrace == -1 || lastBrace == -1) {
+      name.append(path);
+      return;
+    }
+    name.append(path, 0, firstBrace);
+    String tagLists = path.substring(firstBrace + 1, lastBrace);
+    String[] splitPaths = tagLists.split(",");
+    for (String tag : splitPaths) {
+      int equalPos = tag.indexOf("=");
+      String tagKey = tag.substring(0, equalPos);
+      String tagVal = tag.substring(equalPos + 1);
+      ret.put(tagKey, tagVal);
+    }
   }
 }
