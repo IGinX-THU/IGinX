4270c11141cb4d4632fffb9f869821931cd5b671
An Qi <aqnin@outlook.com>
2024-04-01 15:07:40 +0800

    Revert "fix"
    This reverts commit 5f8cf83541f6ce533265dcad9206bce295b2fbdb.

--- dataSources/parquet/src/main/java/cn/edu/tsinghua/iginx/parquet/manager/data/ParquetReadWriter.java
+++ dataSources/parquet/src/main/java/cn/edu/tsinghua/iginx/parquet/manager/data/ParquetReadWriter.java
@@ -89,7 +89,8 @@ public class ParquetReadWriter implements ReadWriter<Long, String, DataType, Obj
     builder.withPageSize(shared.getStorageProperties().getParquetPageSize());
     builder.withCodecFactory(
         allocator,
-        shared.getStorageProperties().getZstdLevel());
+        shared.getStorageProperties().getZstdLevel(),
+        shared.getStorageProperties().getZstdWorkers());
     builder.withCodec(shared.getStorageProperties().getParquetCompression());
     builder.withAllocator(allocator);
 
