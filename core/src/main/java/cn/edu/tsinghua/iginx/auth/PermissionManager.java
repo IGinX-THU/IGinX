c30dfa97ff4ede125127c2492becca800df7eb0f
An Qi <aqnin@outlook.com>
2024-04-01 18:21:41 +0800

    entry and deps

--- core/src/main/java/cn/edu/tsinghua/iginx/auth/PermissionManager.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/auth/PermissionManager.java
@@ -1,5 +1,12 @@
 package cn.edu.tsinghua.iginx.auth;
 
 public class PermissionManager {
-
+//  final Map<String, Properties> loggers = PropertiesUtil.partitionOnCommonPrefixes(
+//      PropertiesUtil.extractSubset(rootProperties, "logger"), true);
+//            for (final Map.Entry<String, Properties> entry : loggers.entrySet()) {
+//    final String name = entry.getKey().trim();
+//    if (!name.equals(LoggerConfig.ROOT)) {
+//      builder.add(createLogger(name, entry.getValue()));
+//    }
+//  }
 }
