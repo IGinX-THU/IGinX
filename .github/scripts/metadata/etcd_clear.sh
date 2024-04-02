0b2122fb26982912cec42fe51642f1a2d570fab6
shinyano <12236590+shinyano@users.noreply.github.com>
2024-04-02 16:27:23 +0800

    test etcd running

---
+++ .github/scripts/metadata/etcd_clear.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+sh -c "ls go/bin"
+
+os=$(uname -s)
+
+kill_process_on_port() {
+    port=$1
+    case $os in
+        Linux|Darwin)
+            pid=$(lsof -ti:$port)
+            if [ ! -z "$pid" ]; then
+                echo "Killing etcd process $pid on port $port"
+                kill -9 $pid
+            else
+                echo "No process found listening on port $port"
+            fi
+            ;;
+        CYGWIN*|MINGW32*|MSYS*|Windows_NT)
+            pid=$(netstat -ano | findstr :$port | awk '{print $5}' | uniq)
+            if [ ! -z "$pid" ]; then
+                echo "Killing etcd process $pid on port $port"
+                Taskkill /PID $pid /F
+            else
+                echo "No process found listening on port $port"
+            fi
+            ;;
+        *)
+            echo "Unsupported OS: $os"
+            ;;
+    esac
+}
+
+kill_process_on_port 2379
+
+sh -c "sleep 2"
+
+sh -c "rm -rf go/bin/*.etcd"
