dd1dcf8037270bed6d616625f0eb32f9455ab37b
RemHero <1104304963@qq.com>
2024-04-11 16:57:06 +0800

    按要求修改

--- shared/src/main/java/cn/edu/tsinghua/iginx/exception/package-info.java
+++ shared/src/main/java/cn/edu/tsinghua/iginx/exception/package-info.java
@@ -1,11 +1,11 @@
 /**
+ * IGinX异常处理规范。
  *
+ * <p><b>core模块</b>
  *
- * <h1>IGinX异常处理规范 </h1>
+ * <p>IginxException是IGinX中在core模块下所有自定义异常的基类。
  *
- * <h2>core模块 </h2>
- *
- * IginxException是IGinX中在core模块下所有自定义异常的基类。
+ * <blockquote>
  *
  * <pre>
  * Exception
@@ -17,28 +17,40 @@
  *     └─SQLParserException        // sql目录下异常基类
  * </pre>
  *
- * <h2>session模块 </h2>
+ * </blockquote>
  *
- * SessionException是session模块下所有自定义异常的基类。
+ * <p><b>session模块</b>
+ *
+ * <p>SessionException是session模块下所有自定义异常的基类。
+ *
+ * <blockquote>
  *
  * <pre>
  * Exception
  * └─SessionException
  * </pre>
  *
- * <h2>client模块 </h2>
+ * </blockquote>
  *
- * ClientException是client模块下所有自定义异常的基类。
+ * <p><b>client模块</b>
+ *
+ * <p>ClientException是client模块下所有自定义异常的基类。
+ *
+ * <blockquote>
  *
  * <pre>
  * Exception
  * └─ClientException
  * </pre>
  *
- * <h2>dataSources模块 </h2>
+ * </blockquote>
  *
- * dataSources下各个数据库自定义异常，但是每个数据库的异常都继承PhysicalException。 每个对接层不能直接用 PhysicalException
+ * <p><b>dataSources模块</b>
+ *
+ * <p>dataSources下各个数据库自定义异常，但是每个数据库的异常都继承PhysicalException。 每个对接层不能直接用 PhysicalException
  * ，每个对接层模块需要自己定义该模块异常。比如，PostgreSQLException，需要继承PhysicalException。
+ *
+ * <blockquote>
  *
  * <pre>
  * Exception
@@ -48,41 +60,59 @@
  *     └─IoTDBException
  * </pre>
  *
- * <h3>Filesystem模块 </h3>
+ * </blockquote>
  *
- * FilesystemException是filesystem模块下所有自定义异常的基类。
+ * <p><b>Filesystem模块</b>
+ *
+ * <p>FilesystemException是filesystem模块下所有自定义异常的基类。
+ *
+ * <blockquote>
  *
  * <pre>
  * PhysicalException
  * └─FilesystemException
  * </pre>
  *
- * <h3>IoTDB12模块 </h3>
+ * </blockquote>
  *
- * IoTDBException是IoTDB12模块下所有自定义异常的基类。
+ * <p><b>IoTDB12模块</b>
+ *
+ * <p>IoTDBException是IoTDB12模块下所有自定义异常的基类。
+ *
+ * <blockquote>
  *
  * <pre>
  * PhysicalException
  * └─IoTDBException
  * </pre>
  *
- * <h3>PostgreSQL模块 </h3>
+ * </blockquote>
  *
- * PostgreSQLException是PostgreSQL模块下所有自定义异常的基类。
+ * <p><b>PostgreSQL模块</b>
+ *
+ * <p>PostgreSQLException是PostgreSQL模块下所有自定义异常的基类。
+ *
+ * <blockquote>
  *
  * <pre>
  * PhysicalException
  * └─PostgreSQLException
  * </pre>
  *
- * <h2>异常处理最佳实践 </h2>
+ * </blockquote>
+ *
+ * <p><b>异常处理最佳实践</b>
  *
  * <ol>
  *   <li>使用 try-with-resource 自动关闭资源。
  *   <li>捕获异常后使用描述性语言记录错误信息，如果是调用外部服务最好包括入参和出参：
- *       <pre>{@code logger.error("说明信息，异常信息：{}", e.getMessage(), e);}</pre>
+ *       <blockquote>
+ *       {@code logger.error("说明信息，异常信息：{}", e.getMessage(), e);}
+ *       </blockquote>
  *   <li>不要同时记录和抛出异常，因为异常会被打印多次。正确的处理方式要么抛出异常要么记录异常，如果抛出异常，不要原封不动地抛出，可以自定义异常抛出：
- *       <pre>{@code throw new MyException("my exception", e);}</pre>
+ *       <blockquote>
+ *       {@code throw new MyException("my exception", e);}
+ *       </blockquote>
  *   <li>自定义异常不要丢弃原有异常，应该将原始异常传入自定义异常中。
  *   <li>自定义异常尽量不要使用检查异常。
  *   <li>尽可能晚的捕获异常，如非必要，建议所有的异常都不要在下层捕获，而应该由最上层捕获并统一处理这些异常。
