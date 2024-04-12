b8e48891f9b8768aed75099969cddacfa3530862
An Qi <aqnin@outlook.com>
2024-04-12 15:10:45 +0800

    fix test

--- shared/src/test/java/cn/edu/tsinghua/iginx/utils/EscaperTest.java
+++ shared/src/test/java/cn/edu/tsinghua/iginx/utils/EscaperTest.java
@@ -11,47 +11,23 @@ public class EscaperTest {
 
   @Test
   public void escape() {
-    {
-      Map<Character, Character> replacementMap = new HashMap<>();
-      replacementMap.put('\\', '\\');
-      replacementMap.put('$', '!');
-      Escaper escaper = new Escaper('\\', replacementMap);
+    Map<Character, Character> replacementMap = new HashMap<>();
+    replacementMap.put('\\', '\\');
+    replacementMap.put('$', '!');
+    Escaper escaper = new Escaper('\\', replacementMap);
 
-      assertEquals("hello\\!world", escaper.escape("hello$world"));
-      assertEquals("hello\\\\world", escaper.escape("hello\\world"));
-    }
-
-    try {
-      Map<Character, Character> replacementMap = new HashMap<>();
-      replacementMap.put('$', '!');
-      Escaper escaper = new Escaper('\\', replacementMap);
-      escaper.escape("hello\\world");
-      fail("should throw IllegalArgumentException");
-    } catch (IllegalArgumentException e) {
-      assertEquals("replacementMap should contain escape character", e.getMessage());
-    }
+    assertEquals("hello\\!world", escaper.escape("hello$world"));
+    assertEquals("hello\\\\world", escaper.escape("hello\\world"));
   }
 
   @Test
   public void unescape() throws ParseException {
-    {
-      Map<Character, Character> replacementMap = new HashMap<>();
-      replacementMap.put('\\', '\\');
-      replacementMap.put('$', '!');
-      Escaper escaper = new Escaper('\\', replacementMap);
+    Map<Character, Character> replacementMap = new HashMap<>();
+    replacementMap.put('\\', '\\');
+    replacementMap.put('$', '!');
+    Escaper escaper = new Escaper('\\', replacementMap);
 
-      assertEquals("hello$world", escaper.unescape("hello\\!world"));
-      assertEquals("hello\\world", escaper.unescape("hello\\\\world"));
-    }
-
-    try {
-      Map<Character, Character> replacementMap = new HashMap<>();
-      replacementMap.put('$', '!');
-      Escaper escaper = new Escaper('\\', replacementMap);
-      escaper.unescape("hello\\world");
-      fail("should throw ParseException");
-    } catch (Exception e) {
-      assertEquals("Invalid escape character", e.getMessage());
-    }
+    assertEquals("hello$world", escaper.unescape("hello\\!world"));
+    assertEquals("hello\\world", escaper.unescape("hello\\\\world"));
   }
 }
