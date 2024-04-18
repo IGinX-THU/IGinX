ccb563937a24c065c338136f41636cae03758648
Janet731 <wjy_20020731@163.com>
2024-04-18 10:40:50 +0800

    add polystore-benchmark-test.yml and mongo and pg engines

---
+++ .github/scripts/benchmarks/mongodb.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+if [ "$RUNNER_OS" == "Linux" ]; then
+  set -e
+  sed -i "s/storageEngineList=127.0.0.1#6667/#storageEngineList=127.0.0.1#6667/g" conf/config.properties
+  sed -i "s/#storageEngineList=127.0.0.1#27017/storageEngineList=127.0.0.1#27017/g" conf/config.properties
+  sh -c "wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-6.0.4.tgz"
+  sh -c "tar -zxf mongodb-linux-x86_64-ubuntu2204-6.0.4.tgz"
+  sudo sh -c "cp -r mongodb-linux-x86_64-ubuntu2204-6.0.4/ mongodb-linux-x86_64-ubuntu2204-6.0.4-$port/"
+  sudo sh -c "cd mongodb-linux-x86_64-ubuntu2204-6.0.4-$port/; mkdir -p data/db; mkdir -p data/log; nohup ./bin/mongod --port $port --dbpath data/db --logpath data/log/mongo.log &"
+elif [ "$RUNNER_OS" == "Windows" ]; then
+  set -e
+  echo "Downloading zip archive. This may take a few minutes..."
+  sh -c "curl -LJO https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-6.0.12.zip -o mongodb-windows-x86_64-6.0.12.zip"
+  sh -c "unzip -qq mongodb-windows-x86_64-6.0.12.zip"
+  echo "Download finished."
+  sh -c "ls mongodb-win32-x86_64-windows-6.0.12/bin"
+  sed -i "s/storageEngineList=127.0.0.1#6667/#storageEngineList=127.0.0.1#6667/g" conf/config.properties
+  sed -i "s/#storageEngineList=127.0.0.1#27017/storageEngineList=127.0.0.1#27017/g" conf/config.properties
+  sh -c "cp -r mongodb-win32-x86_64-windows-6.0.12/ mongodb-win32-x86_64-windows-6.0.12-$port/"
+  sh -c "cd mongodb-win32-x86_64-windows-6.0.12-$port/; mkdir -p data/db; mkdir -p logs; "
+  filePrefix="mongodb-win32-x86_64-windows-6.0.12-$port"
+  arguments="-ArgumentList '--port', '$port', '--dbpath', '$filePrefix/data/db', '--logpath', '$filePrefix/logs/db.log'"
+  powershell -command "Start-Process -FilePath '$filePrefix/bin/mongod' $arguments"
+elif [ "$RUNNER_OS" == "macOS" ]; then
+  set -e
+  sed -i "" "s/storageEngineList=127.0.0.1#6667/#storageEngineList=127.0.0.1#6667/" conf/config.properties
+  sed -i "" "s/#storageEngineList=127.0.0.1#27017/storageEngineList=127.0.0.1#27017/" conf/config.properties
+  sh -c "wget https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-6.0.4.tgz"
+  sh -c "tar -zxf mongodb-macos-x86_64-6.0.4.tgz"
+  sudo sh -c "cp -r mongodb-macos-x86_64-6.0.4/ mongodb-macos-x86_64-6.0.4-$port/"
+  sudo sh -c "cd mongodb-macos-x86_64-6.0.4-$port/; mkdir -p data/db; mkdir -p data/log; nohup ./bin/mongod --port $port --dbpath data/db --logpath data/log/mongo.log &"
+else
+  echo "$RUNNER_OS is not supported"
+  exit 1
+fi
---
+++ .github/scripts/benchmarks/postgres.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+if [ "$RUNNER_OS" == "Linux" ]; then
+  set -e
+  sed -i "s/storageEngineList=127.0.0.1#6667#iotdb12/#storageEngineList=127.0.0.1#6667#iotdb12/g" conf/config.properties
+  sed -i "s/#storageEngineList=127.0.0.1#5432#postgresql/storageEngineList=127.0.0.1#5432#postgresql/g" conf/config.properties
+  sh -c "sudo sh -c 'echo \"deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main\" > /etc/apt/sources.list.d/pgdg.list'"
+  sh -c "wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -"
+  sh -c "sudo apt-get update"
+  sh -c "sudo apt-get -y install postgresql-15"
+  sh -c "sudo rm -rf /var/lib/postgresql/15/main"
+  sh -c "sudo mkdir -p /var/lib/postgresql/15/main"
+  sh -c "sudo chown -R postgres /var/lib/postgresql/15/main"
+  sh -c "sudo chmod -R 777 /var/lib/postgresql/15/main"
+  sh -c "sudo chmod -R 777 /usr/lib/postgresql/15"
+  sh -c "sudo mkdir -p /usr/lib/postgresql-5432"
+  sh -c "sudo chmod -R 777 /usr/lib/postgresql-5432"
+  sh -c "sudo cp -R /usr/lib/postgresql/15 /usr/lib/postgresql-5432"
+  sh -c "sudo mkdir -p /var/lib/postgresql-5432/15/main"
+  sh -c "sudo chown -R postgres /var/lib/postgresql-5432/15/main"
+  sh -c "sudo chmod -R 777 /var/lib/postgresql-5432/15/main"
+  sh -c "sudo su - postgres -c '/usr/lib/postgresql-5432/15/bin/initdb -D /var/lib/postgresql-5432/15/main --auth trust --no-instructions'"
+  sh -c "sudo su - postgres -c '/usr/lib/postgresql-5432/15/bin/pg_ctl -D /var/lib/postgresql-5432/15/main -o \"-F -p 5432\" start'"
+  sh -c "sudo su - postgres -c '/usr/lib/postgresql-5432/15/bin/psql -c \"ALTER USER postgres WITH PASSWORD '\''postgres'\'';\"'"
+elif [ "$RUNNER_OS" == "Windows" ]; then
+  set -e
+  echo "Downloading zip archive. This may take a few minutes..."
+  sh -c "curl -LJO https://get.enterprisedb.com/postgresql/postgresql-15.5-1-windows-x64-binaries.zip -o postgresql-15.5-1-windows-x64-binaries.zip"
+  sh -c "unzip -qq postgresql-15.5-1-windows-x64-binaries.zip"
+  echo "Download finished."
+  sh -c "ls pgsql/bin"
+  sed -i "s/storageEngineList=127.0.0.1#6667#iotdb12/#storageEngineList=127.0.0.1#6667#iotdb12/g" conf/config.properties
+  sed -i "s/#storageEngineList=127.0.0.1#5432#postgresql/storageEngineList=127.0.0.1#5432#postgresql/g" conf/config.properties
+  sh -c "cp -R pgsql pgsql-5432"
+  filePrefix="pgsql-5432"
+  sh -c "mkdir -p $filePrefix/data"
+  sh -c "mkdir -p $filePrefix/logs"
+  arguments="-ArgumentList '-D', '$filePrefix/data', '--username=postgres', '--auth', 'trust', '--no-instructions', '-E', 'UTF8'"
+  redirect="-RedirectStandardOutput '$filePrefix/logs/db.log' -RedirectStandardError '$filePrefix/logs/db-error.log'"
+  powershell -command "Start-Process -FilePath '$filePrefix/bin/initdb' -NoNewWindow $arguments $redirect"
+  ctlarguments="-ArgumentList '-D', '$filePrefix/data', '-o', '\"-F -p 5432\"', 'start'"
+  ctlredirect="-RedirectStandardOutput '$filePrefix/logs/pg_ctl.log' -RedirectStandardError '$filePrefix/logs/pg_ctl-error.log'"
+  sh -c "sleep 6"
+  powershell -command "Start-Process -FilePath '$filePrefix/bin/pg_ctl' -NoNewWindow $ctlarguments $ctlredirect"
+  sql='"ALTER USER postgres WITH PASSWORD '\'\''postgres'\'\'';"'
+  sqlarguments="-ArgumentList '-c', $sql"
+  sqlredirect="-RedirectStandardOutput '$filePrefix/logs/psql.log' -RedirectStandardError '$filePrefix/logs/psql-error.log'"
+  powershell -command "Start-Process -FilePath '$filePrefix/bin/psql' -NoNewWindow $sqlarguments $sqlredirect"
+elif [ "$RUNNER_OS" == "macOS" ]; then
+  set -e
+  sed -i "" "s/storageEngineList=127.0.0.1#6667#iotdb12/#storageEngineList=127.0.0.1#6667#iotdb12/g" conf/config.properties
+  sed -i "" "s/#storageEngineList=127.0.0.1#5432#postgresql/storageEngineList=127.0.0.1#5432#postgresql/g" conf/config.properties
+  sh -c "wget --quiet https://get.enterprisedb.com/postgresql/postgresql-15.2-1-osx-binaries.zip"
+  sh -c "sudo unzip -q postgresql-15.2-1-osx-binaries.zip"
+  sh -c "sudo dscl . -create /Users/postgres UniqueID 666"
+  sh -c "sudo dscl . -create /Groups/postgres PrimaryGroupID 777"
+  sh -c "sudo dscl . -create /Users/postgres PrimaryGroupID 777"
+  sh -c "sudo dscl . -create /Users/postgres UserShell /bin/bash"
+  sh -c "sudo dscl . -create /Users/postgres RealName \"PostgreSQL\""
+  sh -c "sudo dscl . create /Groups/postgres passwd '*'"
+  sh -c "sudo mkdir /Users/postgres"
+  sh -c "sudo chown -R postgres:postgres /Users/postgres"
+  sh -c "sudo dscl . -create /Users/postgres NFSHomeDirectory /Users/postgres"
+  sh -c "sudo mkdir -p /var/lib/postgresql/15/main"
+  sh -c "sudo chown -R postgres /var/lib/postgresql/15/main"
+  sh -c "sudo chmod -R 777 /var/lib/postgresql/15/main"
+  sh -c "sudo mkdir -p /Users/postgres/pgsql-5432"
+  sh -c "sudo chmod -R 777 /Users/postgres/pgsql-5432"
+  sh -c "sudo cp -R pgsql /Users/postgres/pgsql-5432"
+  sh -c "sudo mkdir -p /var/lib/postgresql-5432/15/main"
+  sh -c "sudo chown -R postgres /var/lib/postgresql-5432/15/main"
+  sh -c "sudo chmod -R 777 /var/lib/postgresql-5432/15/main"
+  sh -c "sudo su - postgres -c '/Users/postgres/pgsql-5432/pgsql/bin/initdb -D /var/lib/postgresql-5432/15/main --auth trust --no-instructions'"
+  sh -c "sudo su - postgres -c '/Users/postgres/pgsql-5432/pgsql/bin/pg_ctl -D /var/lib/postgresql-5432/15/main -o \"-F -p 5432\" start'"
+  sh -c "sudo su - postgres -c '/Users/postgres/pgsql-5432/pgsql/bin/psql -c \"ALTER USER postgres WITH PASSWORD '\''postgres'\'';\"'"
+else
+  echo "$RUNNER_OS is not supported"
+  exit 1
+fi
