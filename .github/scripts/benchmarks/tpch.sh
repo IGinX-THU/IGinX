1d670b8f4f7243d65e13a666e54c0b59eb1e1e1a
Janet731 <wjy_20020731@163.com>
2024-04-19 16:55:37 +0800

    add tpch data generater

--- .github/scripts/benchmarks/tpch.sh
+++ .github/scripts/benchmarks/tpch.sh
@@ -3,21 +3,22 @@
 if [ "$RUNNER_OS" = "Windows" ]; then
   python thu_cloud_download.py \
     -l https://cloud.tsinghua.edu.cn/d/63b9d1a6444e47ae8ac5/ \
-    -s  ".."
+    -s  "."
 else
   pwd
+  ls
   python3 thu_cloud_download.py \
     -l https://cloud.tsinghua.edu.cn/d/63b9d1a6444e47ae8ac5/ \
-    -s  ".."
+    -s  "."
 fi
 
-cd ../tpc
+cd tpc
 pwd
 unzip E86C7FBC-89AB-4825-BAFA-5CF73EC94ED3-TPC-H-Tool.zip
 rm E86C7FBC-89AB-4825-BAFA-5CF73EC94ED3-TPC-H-Tool.zip
 echo "TPCH数据生成工具下载完成"
 
-cd ../tpc/TPC-H\ V3.0.1/dbgen
+cd TPC-H\ V3.0.1/dbgen
 if [ "$RUNNER_OS" = "Linux" ]; then
   apt install gcc
   sudo apt-get update
