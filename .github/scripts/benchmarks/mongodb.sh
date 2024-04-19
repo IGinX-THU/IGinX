7a011f25189e1b9dc8abd520f324243a117d5df5
Janet731 <wjy_20020731@163.com>
2024-04-19 16:09:27 +0800

    debug

--- .github/scripts/benchmarks/mongodb.sh
+++ .github/scripts/benchmarks/mongodb.sh
@@ -5,8 +5,8 @@ if [ "$RUNNER_OS" = "Linux" ]; then
   sed -i "s/#storageEngineList=127.0.0.1#27017/storageEngineList=127.0.0.1#27017/g" conf/config.properties
   sh -c "wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-6.0.4.tgz"
   sh -c "tar -zxf mongodb-linux-x86_64-ubuntu2204-6.0.4.tgz"
-  sudo sh -c "cp -r mongodb-linux-x86_64-ubuntu2204-6.0.4/ mongodb-linux-x86_64-ubuntu2204-6.0.4-$port/"
-  sudo sh -c "cd mongodb-linux-x86_64-ubuntu2204-6.0.4-$port/; mkdir -p data/db; mkdir -p data/log; nohup ./bin/mongod --port $port --dbpath data/db --logpath data/log/mongo.log &"
+  sudo sh -c "cp -r mongodb-linux-x86_64-ubuntu2204-6.0.4/ mongodb-linux-x86_64-ubuntu2204-6.0.4-27017/"
+  sudo sh -c "cd mongodb-linux-x86_64-ubuntu2204-6.0.4-27017/; mkdir -p data/db; mkdir -p data/log; nohup ./bin/mongod --port 27017 --dbpath data/db --logpath data/log/mongo.log &"
 elif [ "$RUNNER_OS" = "Windows" ]; then
   set -e
   echo "Downloading zip archive. This may take a few minutes..."
@@ -16,10 +16,10 @@ elif [ "$RUNNER_OS" = "Windows" ]; then
   sh -c "ls mongodb-win32-x86_64-windows-6.0.12/bin"
   sed -i "s/storageEngineList=127.0.0.1#6667/#storageEngineList=127.0.0.1#6667/g" conf/config.properties
   sed -i "s/#storageEngineList=127.0.0.1#27017/storageEngineList=127.0.0.1#27017/g" conf/config.properties
-  sh -c "cp -r mongodb-win32-x86_64-windows-6.0.12/ mongodb-win32-x86_64-windows-6.0.12-$port/"
-  sh -c "cd mongodb-win32-x86_64-windows-6.0.12-$port/; mkdir -p data/db; mkdir -p logs; "
-  filePrefix="mongodb-win32-x86_64-windows-6.0.12-$port"
-  arguments="-ArgumentList '--port', '$port', '--dbpath', '$filePrefix/data/db', '--logpath', '$filePrefix/logs/db.log'"
+  sh -c "cp -r mongodb-win32-x86_64-windows-6.0.12/ mongodb-win32-x86_64-windows-6.0.12-27017/"
+  sh -c "cd mongodb-win32-x86_64-windows-6.0.12-27017/; mkdir -p data/db; mkdir -p logs; "
+  filePrefix="mongodb-win32-x86_64-windows-6.0.12-27017"
+  arguments="-ArgumentList '--port', '27017', '--dbpath', '$filePrefix/data/db', '--logpath', '$filePrefix/logs/db.log'"
   powershell -command "Start-Process -FilePath '$filePrefix/bin/mongod' $arguments"
 elif [ "$RUNNER_OS" = "macOS" ]; then
   set -e
@@ -27,8 +27,8 @@ elif [ "$RUNNER_OS" = "macOS" ]; then
   sed -i "" "s/#storageEngineList=127.0.0.1#27017/storageEngineList=127.0.0.1#27017/" conf/config.properties
   sh -c "wget https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-6.0.4.tgz"
   sh -c "tar -zxf mongodb-macos-x86_64-6.0.4.tgz"
-  sudo sh -c "cp -r mongodb-macos-x86_64-6.0.4/ mongodb-macos-x86_64-6.0.4-$port/"
-  sudo sh -c "cd mongodb-macos-x86_64-6.0.4-$port/; mkdir -p data/db; mkdir -p data/log; nohup ./bin/mongod --port $port --dbpath data/db --logpath data/log/mongo.log &"
+  sudo sh -c "cp -r mongodb-macos-x86_64-6.0.4/ mongodb-macos-x86_64-6.0.4-27017/"
+  sudo sh -c "cd mongodb-macos-x86_64-6.0.4-27017/; mkdir -p data/db; mkdir -p data/log; nohup ./bin/mongod --port 27017 --dbpath data/db --logpath data/log/mongo.log &"
 else
   echo "$RUNNER_OS is not supported"
   exit 1
