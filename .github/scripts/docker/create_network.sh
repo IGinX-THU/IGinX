3c1d03ea0071c119e7d4a5753f1fd0b63d27cdd0
shinyano <12236590+shinyano@users.noreply.github.com>
2024-04-25 09:41:34 +0800

    install docker on macos && more check before docker network create

---
+++ .github/scripts/docker/create_network.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+set -e
+
+docker network ls
+
+output=$(docker network ls | grep ' bridge ')
+
+if [ -z "$output" ]; then
+    docker network create -d nat --attachable --subnet 172.40.0.0/16 docker-cluster-iginx
+else
+    docker network create -d bridge --attachable --subnet 172.40.0.0/16 docker-cluster-iginx
+fi
