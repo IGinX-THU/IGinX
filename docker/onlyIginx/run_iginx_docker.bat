543d773403345046336c208756455db50da37543
shinyano <12236590+shinyano@users.noreply.github.com>
2024-04-25 17:12:49 +0800

    what's wrong

--- docker/onlyIginx/run_iginx_docker.bat
+++ docker/onlyIginx/run_iginx_docker.bat
@@ -1,6 +1,14 @@
 @echo off
+set "current_dir=%CD%"
+@REM 将路径中的单反斜线替换为双反斜线
+set "double_slash_dir=%current_dir:\=\\%"
+
+@REM 初始化变量
+set "name=iginx-client"
+set "logdir=%double_slash_dir%\\..\\..\\logs\\docker_logs"
+
 set ip=%1
 set name=%2
 set port=%3
 mkdir -p logs/docker_logs
-docker run --name="%name%" --privileged -dit --net docker-cluster-iginx --ip %ip% --add-host=host.docker.internal:host-gateway -v logs/docker_logs/:/iginx/logs/ -p %port%:6888 iginx:0.6.0
+docker run --name="%name%" --privileged -dit --net docker-cluster-iginx --ip %ip% --add-host=host.docker.internal:host-gateway -v %logdir%:/iginx/logs/ -p %port%:6888 iginx:0.6.0
--- docker/onlyIginx/run_iginx_docker.sh
+++ docker/onlyIginx/run_iginx_docker.sh
@@ -1,5 +1,6 @@
 ip=$1
 name=$2
 port=$3
-mkdir -p logs/docker_logs
-docker run --name="${name}" --privileged -dit --net docker-cluster-iginx --ip ${ip} --add-host=host.docker.internal:host-gateway -v logs/docker_logs/:/iginx/logs/ -p ${port}:6888 iginx:0.6.0
+logdir="$(pwd)/../../logs/docker_logs"
+mkdir -p $logdir
+docker run --name="${name}" --privileged -dit --net docker-cluster-iginx --ip ${ip} --add-host=host.docker.internal:host-gateway -v ${logdir}:/iginx/logs/ -p ${port}:6888 iginx:0.6.0
