8a693a0bfe992861f90b7fb97844206c75162434
shinyano <12236590+shinyano@users.noreply.github.com>
2024-04-26 22:43:22 +0800

    fix test

--- .github/scripts/test/cli/test_remote_udf_windows.sh
+++ .github/scripts/test/cli/test_remote_udf_windows.sh
@@ -12,7 +12,10 @@ echo "$os"
 
 export MSYS_NO_PATHCONV=1
 # MSYS_NO_PATHCONV=1 : not to convert docker script path to git bash path
-SCRIPT_PREFIX="docker exec iginx-client cmd /c \"C:\\\\iginx_client\\\\sbin\\\\start_cli.bat -h host.docker.internal -e "
+SCRIPT_PREFIX="docker exec iginx-client cmd /c \"C:\\iginx_client\\sbin\\start_cli.bat -h host.docker.internal -e "
+
+#docker exec container_name cmd /c "C:\\iginx_client\\sbin\\start_cli.bat -h host.docker.internal -e \"create function udtf \\\"mock_udf\\\" from \\\"MockUDF\\\" in \\\"../data/udf/mock_udf.py\\\";\""
+
 
 sleep 5
 #
@@ -43,10 +46,14 @@ fi
 
 
 # single udf in one file
-${SCRIPT_PREFIX} "create function udtf \"mock_udf\" from \"MockUDF\" in \"../data/udf/mock_udf.py\";\""
+${SCRIPT_PREFIX} "\\\"create function udtf \"mock_udf\" from \"MockUDF\" in \"../data/udf/mock_udf.py\";\\\""
+echo ${SCRIPT_PREFIX} "\\\"create function udtf \"mock_udf\" from \"MockUDF\" in \"../data/udf/mock_udf.py\";\\\""
 # multiple udfs in one module
-${SCRIPT_PREFIX} "CREATE FUNCTION udtf \"udf_a\" FROM \"my_module.my_class_a.ClassA\", \"udf_b\" FROM \"my_module.my_class_a.ClassB\", \"udf_sub\" FROM \"my_module.sub_module.sub_class_a.SubClassA\" IN \"../data/udf/my_module\";"
+${SCRIPT_PREFIX} "\\\"CREATE FUNCTION udtf \"udf_a\" FROM \"my_module.my_class_a.ClassA\", \"udf_b\" FROM \"my_module.my_class_a.ClassB\", \"udf_sub\" FROM \"my_module.sub_module.sub_class_a.SubClassA\" IN \"../data/udf/my_module\";\\\""
+echo ${SCRIPT_PREFIX} "\\\"CREATE FUNCTION udtf \"udf_a\" FROM \"my_module.my_class_a.ClassA\", \"udf_b\" FROM \"my_module.my_class_a.ClassB\", \"udf_sub\" FROM \"my_module.sub_module.sub_class_a.SubClassA\" IN \"../data/udf/my_module\";\\\""
 # multiple udfs in one file
-${SCRIPT_PREFIX} "CREATE FUNCTION udtf \"udf_a_file\" FROM \"ClassA\", udsf \"udf_b_file\" FROM \"ClassB\", udaf \"udf_c_file\" FROM \"ClassC\" IN \"../data/udf/my_module/idle_classes.py\";"
+${SCRIPT_PREFIX} "\\\"CREATE FUNCTION udtf \"udf_a_file\" FROM \"ClassA\", udsf \"udf_b_file\" FROM \"ClassB\", udaf \"udf_c_file\" FROM \"ClassC\" IN \"../data/udf/my_module/idle_classes.py\";\\\""
+echo ${SCRIPT_PREFIX} "\\\"CREATE FUNCTION udtf \"udf_a_file\" FROM \"ClassA\", udsf \"udf_b_file\" FROM \"ClassB\", udaf \"udf_c_file\" FROM \"ClassC\" IN \"../data/udf/my_module/idle_classes.py\";\\\""
 
-${SCRIPT_PREFIX} "show functions;"
+${SCRIPT_PREFIX} "\\\"show functions;\\\""
+echo ${SCRIPT_PREFIX} "\\\"show functions;\\\""
