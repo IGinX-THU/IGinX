91a104a94c2f73ca8f72f6ce93a89f93a279f28b
Janet731 <wjy_20020731@163.com>
2024-04-26 19:04:59 +0800

    debug

--- .github/scripts/benchmarks/tpch.sh
+++ .github/scripts/benchmarks/tpch.sh
@@ -26,6 +26,9 @@ if [ "$RUNNER_OS" = "Linux" ]; then
 
 elif [ "$RUNNER_OS" = "Windows" ]; then
   echo "windows"
+  cp makefile.suite makefile
+  awk 'NR < 103 || NR > 111 { print } NR == 103 { print "CC      = gcc\n# Current values for DATABASE are: INFORMIX, DB2, TDAT (Teradata)\n#                                  SQLSERVER, SYBASE, ORACLE, VECTORWISE\n# Current values for MACHINE are:  ATT, DOS, HP, IBM, ICL, MVS, \n#                                  SGI, SUN, U2200, VMS, LINUX, WIN32 \n# Current values for WORKLOAD are:  TPCH\nDATABASE= SQLSERVER\nMACHINE = WIN32\nWORKLOAD = TPCH" }' makefile > new_makefile
+  mv new_makefile makefile
 
 elif [ "$RUNNER_OS" = "macOS" ]; then
   # 根据 https://blog.csdn.net/mei86233824/article/details/81066999 修改makefile文件并进行编译生成可执行文件
