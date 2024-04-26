d040bfe8a96335f5762defeb1134a6226bb1c1a9
Janet731 <wjy_20020731@163.com>
2024-04-26 21:05:02 +0800

    changed data cleaning

--- .github/scripts/benchmarks/dataCleaning.sh
+++ .github/scripts/benchmarks/dataCleaning.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if [ "$RUNNER_OS" = "Linux" ]; then
-  python3 dataCleaning/gen_data.py -n 1000000
-elif [ "$RUNNER_OS" = "Windows" ]; then
-  python dataCleaning/gen_data.py -n 1000000
-elif [ "$RUNNER_OS" = "macOS" ]; then
-  python3 dataCleaning/gen_data.py -n 1000000
-fi
 set -e
 
 COMMAND1='LOAD DATA FROM INFILE "../IGinX/dataCleaning/zipcode_city.csv" AS CSV INTO uszip(key,city,zipcode);SELECT count(a.zipcode) FROM uszip as a JOIN uszip as b ON a.zipcode = b.zipcode WHERE a.city <> b.city;'
@@ -14,7 +7,7 @@ COMMAND1='LOAD DATA FROM INFILE "../IGinX/dataCleaning/zipcode_city.csv" AS CSV 
 SCRIPT_COMMAND="bash client/target/iginx-client-0.6.0-SNAPSHOT/sbin/start_cli.sh -e '{}'"
 
 bash -c "chmod +x client/target/iginx-client-0.6.0-SNAPSHOT/sbin/start_cli.sh"
-
+RUNNER_OS='macOS'
 if [ "$RUNNER_OS" = "Linux" ]; then
   bash -c "echo '$COMMAND1' | xargs -0 -t -i ${SCRIPT_COMMAND}"
 elif [ "$RUNNER_OS" = "Windows" ]; then
---
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/polybench/DataCleaningRunner.py
@@ -0,0 +1,45 @@
+import subprocess
+
+# 生成 zipcode -> city的映射关系，error rate 10%
+# lineNum 为 -n 后面的参数
+import argparse
+parser = argparse.ArgumentParser(description='generate zipcode -> city data for test')
+parser.add_argument('-n', dest='lineNum', type=int, default=1000000,
+                    help='line number of the generated data file, default 1000000')
+
+args = parser.parse_args()
+print('Data generated! Line number: ', args.lineNum)
+if args.lineNum < 0:
+    print('LineNum should be a positive number')
+    exit(1)
+elif args.lineNum > 10000000:
+    print('LineNum too big, should be less than 10000000')
+    exit(1)
+
+correctNum = args.lineNum - args.lineNum // 10
+
+zipcodes = [i for i in range(correctNum)]
+cities = ['city' + str(i) for i in range(args.lineNum)]
+# zipcodes加入10%的重复
+zipcodes = zipcodes + [zipcodes[i] for i in range(args.lineNum // 10)]
+with open('dataCleaning/zipcode_city.csv', 'w') as f:
+    for i in range(args.lineNum):
+        f.write(str(i) + ',' + cities[i] + ',' + str(zipcodes[i]) + '\n')
+
+# 要运行的 shell 脚本文件路径
+script_path = ".github/scripts/benchmarks/dataCleaning.sh"
+
+# 使用 subprocess.run() 运行 shell 脚本
+# shell=True 表示通过 shell 解释器执行脚本
+# 如果脚本有输出，可以通过 stdout=subprocess.PIPE 捕获输出
+result = subprocess.run(script_path, shell=True, stdout=subprocess.PIPE, text=True)
+
+# 检查脚本是否成功执行
+if result.returncode == 0:
+    print("Shell 脚本执行成功！")
+    # 如果脚本有输出，可以打印输出内容
+    if result.stdout:
+        print("脚本输出：", result.stdout)
+    # TODO 正确性检验
+else:
+    print("Shell 脚本执行失败。")
