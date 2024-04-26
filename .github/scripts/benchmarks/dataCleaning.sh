6aa0f5d88c0aa0a86846acaef0a06903572334c9
Janet731 <wjy_20020731@163.com>
2024-04-26 21:20:49 +0800

    changed data cleaning

--- .github/scripts/benchmarks/dataCleaning.sh
+++ .github/scripts/benchmarks/dataCleaning.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-COMMAND1='LOAD DATA FROM INFILE "../IGinX/dataCleaning/zipcode_city.csv" AS CSV INTO uszip(key,city,zipcode);SELECT count(a.zipcode) FROM uszip as a JOIN uszip as b ON a.zipcode = b.zipcode WHERE a.city <> b.city;'
+COMMAND1='LOAD DATA FROM INFILE "dataCleaning/zipcode_city.csv" AS CSV INTO uszip(key,city,zipcode);SELECT count(a.zipcode) FROM uszip as a JOIN uszip as b ON a.zipcode = b.zipcode WHERE a.city <> b.city;'
 
 SCRIPT_COMMAND="bash client/target/iginx-client-0.6.0-SNAPSHOT/sbin/start_cli.sh -e '{}'"
 
 bash -c "chmod +x client/target/iginx-client-0.6.0-SNAPSHOT/sbin/start_cli.sh"
-RUNNER_OS='macOS'
+
 if [ "$RUNNER_OS" = "Linux" ]; then
   bash -c "echo '$COMMAND1' | xargs -0 -t -i ${SCRIPT_COMMAND}"
 elif [ "$RUNNER_OS" = "Windows" ]; then
