95bb1733926dc1ca3cb3f262b34b7cd7d7e005a0
shinyano <12236590+shinyano@users.noreply.github.com>
2024-04-26 17:09:58 +0800

    iginx revert to local service

--- .github/scripts/test/cli/test_remote_udf.sh
+++ .github/scripts/test/cli/test_remote_udf.sh
@@ -9,34 +9,34 @@ ls docker/client/data/udf
 
 export MSYS_NO_PATHCONV=1
 # MSYS_NO_PATHCONV=1 : not to convert docker script path to git bash path
-SCRIPT_PREFIX="docker exec iginx-client /iginx_client/sbin/start_cli.sh -h 172.40.0.2 -e"
+SCRIPT_PREFIX="docker exec iginx-client /iginx_client/sbin/start_cli.sh -h host.docker.internal -e"
 
-sleep 15
-
-docker ps
-docker network inspect docker-cluster-iginx
-ls logs/docker_logs
-cat logs/docker_logs/*
-docker exec iginx0 cat /logs/iginx-latest.log
-
-timeout=30
-interval=2
-
-elapsed_time=0
-while [ $elapsed_time -lt $timeout ]; do
-  output=$(${SCRIPT_PREFIX} "show cluster info;")
-  if [[ $output =~ 'Connection refused (Connection refused)' ]]; then
-      echo "$output"
-      sleep $interval
-  else
-      break
-  fi
-  elapsed_time=$((elapsed_time + interval))
-done
-if [[ $output =~ 'Connection refused (Connection refused)' ]]; then
-  echo "IGinX not reachable"
-  exit 1
-fi
+#sleep 15
+#
+#docker ps
+#docker network inspect docker-cluster-iginx
+#ls logs/docker_logs
+#cat logs/docker_logs/*
+#docker exec iginx0 cat /logs/iginx-latest.log
+#
+#timeout=30
+#interval=2
+#
+#elapsed_time=0
+#while [ $elapsed_time -lt $timeout ]; do
+#  output=$(${SCRIPT_PREFIX} "show cluster info;")
+#  if [[ $output =~ 'Connection refused (Connection refused)' ]]; then
+#      echo "$output"
+#      sleep $interval
+#  else
+#      break
+#  fi
+#  elapsed_time=$((elapsed_time + interval))
+#done
+#if [[ $output =~ 'Connection refused (Connection refused)' ]]; then
+#  echo "IGinX not reachable"
+#  exit 1
+#fi
 
 
 # single udf in one file
