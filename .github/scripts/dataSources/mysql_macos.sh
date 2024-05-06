3b30e8106189b4970fde6833d8e6170e66820869
RemHero <1104304963@qq.com>
2024-05-06 10:43:57 +0800

    enhance the test flow

--- .github/scripts/dataSources/mysql_macos.sh
+++ .github/scripts/dataSources/mysql_macos.sh
@@ -5,3 +5,20 @@ set -e
 sed -i "" "s/storageEngineList=127.0.0.1#6667#iotdb12/#storageEngineList=127.0.0.1#6667#iotdb12/g" conf/config.properties
 
 sed -i  "" "s^#storageEngineList=127.0.0.1#3306#relational#engine=mysql#username=root#password=mysql#has_data=false#meta_properties_path=your-meta-properties-path^storageEngineList=127.0.0.1#3306#relational#engine=mysql#username=root#has_data=false#meta_properties_path=/Users/runner/work/IGinX/IGinX/dataSources/relational/src/main/resources/mysql-meta-template.properties^g" conf/config.properties
+
+brew install mysql@8.0
+
+for port in "$@"
+do
+    mkdir -p ./data${port}
+    sudo touch /usr/local/etc/my${port}.cnf
+    sudo sh -c "echo '[mysqld]' > /usr/local/etc/my${port}.cnf"
+    sudo sh -c "echo 'port=${port}' >> /usr/local/etc/my${port}.cnf"
+    sudo sh -c "echo 'mysqlx_port=$((33060+${port}-3306))' >> /usr/local/etc/my${port}.cnf"
+    sudo sh -c "echo 'user=root' >> /usr/local/etc/my${port}.cnf"
+    sudo sh -c "echo 'socket=/tmp/mysql${port}.sock' >> /usr/local/etc/my${port}.cnf"
+
+    /usr/local/Cellar/mysql@8.0/8.0.36_1/bin/mysqld --defaults-file=/usr/local/etc/my${port}.cnf --mysqlx=0 --user=root --initialize-insecure --datadir=./data${port} --port=${port}
+
+    /usr/local/Cellar/mysql@8.0/8.0.36_1/bin/mysqld --defaults-file=/usr/local/etc/my${port}.cnf --mysqlx=0 --datadir=./data${port} --port=${port} &
+done
--- .github/scripts/dataSources/mysql_windows.sh
+++ .github/scripts/dataSources/mysql_windows.sh
@@ -5,3 +5,20 @@ set -e
 sed -i "s/storageEngineList=127.0.0.1#6667#iotdb12/#storageEngineList=127.0.0.1#6667#iotdb12/g" conf/config.properties
 
 sed -i "s^#storageEngineList=127.0.0.1#3306#relational#engine=mysql#username=root#password=mysql#has_data=false#meta_properties_path=your-meta-properties-path^storageEngineList=127.0.0.1#3306#relational#engine=mysql#username=root#has_data=false#meta_properties_path=D:/a/IGinX/IGinX/dataSources/relational/src/main/resources/mysql-meta-template.properties^g" conf/config.properties
+
+choco install mysql --version 8.0.26 --params "/InstallLocation:./"
+
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'mysql';" > init.txt
+
+for port in "$@"
+do
+    mkdir ./mysql/data${port}
+    ./mysql/mysql-8.0.26-winx64/bin/mysqld --initialize-insecure --datadir=./mysql/data$port --port=$port --console
+    ./mysql/mysql-8.0.26-winx64/bin/mysqld --console --datadir=./mysql/data$port --port=$port &
+done
+
+for port in "$@"
+do
+    ./mysql/mysql-8.0.26-winx64/bin/mysql -v -u root --port=$port
+done
+
