55c2a89c695f77ae6eecd72d506dbefea2bf4a2a
shinyano <12236590+shinyano@users.noreply.github.com>
2024-05-11 13:01:10 +0800

    macos use local ip address

--- .github/scripts/test/cli/test_remote_udf_macos.sh
+++ .github/scripts/test/cli/test_remote_udf_macos.sh
@@ -12,27 +12,30 @@ echo "$os"
 
 ifconfig
 
-## 找到docker nat adapter的ip4地址
-#readarray -t results < <(ipconfig)
-#adapterfound=false
-#trimmed_string=notFound
-#
-#for line in "${results[@]}"; do
-#	if [[ $line =~ "vEthernet (bridge)" ]]; then
-#		adapterfound=true
-#	elif [[ "${adapterfound}" == "true" && $line =~ "IPv4" ]]; then
-#		IFS=':'
-#		read -ra arr <<< "$line"
-#		trimmed_string=$(echo ${arr[1]} | tr -d ' ')
-#		echo $trimmed_string
-#		adapterfound=false
-#	fi
-#done
-#
-#if [[ "$line" == "notFound" ]]; then
-#    echo "ip4 addr for host not found"
-#    exit 1
-#fi
+# 找到本机的ip4地址
+readarray -t results < <(ifconfig)
+adapterfound=false
+trimmed_string=notFound
+
+for line in "${results[@]}"; do
+	if [[ $line =~ "en0:" ]]; then
+		adapterfound=true
+	elif [[ "${adapterfound}" == "true" && $line =~ "inet " ]]; then
+		IFS=' '
+		read -ra arr <<< "$line"
+		trimmed_string=$(echo ${arr[1]} | tr -d ' ')
+		echo $trimmed_string
+		adapterfound=false
+	fi
+done
+
+if [[ "$line" == "notFound" ]]; then
+    echo "ip4 addr for host not found"
+    exit 1
+fi
+
+docker exec iginx-client ping host.docker.internal
+docker exec iginx-client ping ${trimmed_string}
 
 export MSYS_NO_PATHCONV=1
 # MSYS_NO_PATHCONV=1 : not to convert docker script path to git bash path
