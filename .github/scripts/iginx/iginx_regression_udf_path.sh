e3f7f48f32c10fb3989ea3821a8790c81e05e4d6
Janet731 <wjy_20020731@163.com>
2024-05-21 11:21:31 +0800

    debug

---
+++ .github/scripts/iginx/iginx_regression_udf_path.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cd IGinX
+pwd
+set -e
+
+cd core/target/iginx-core-0.6.0-SNAPSHOT/
+
+iginx_home_path=$PWD
+
+cd ..
+
+if [ -n "$MSYSTEM" ]; then
+    windows_path=$(cygpath -w "$iginx_home_path")
+
+    export IGINX_HOME=$windows_path
+
+    powershell -Command "Start-Process -FilePath 'iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.bat' -NoNewWindow -RedirectStandardOutput '../../iginx-udf.log' -RedirectStandardError '../../iginx-udf-error.log'"
+else
+    export IGINX_HOME=$iginx_home_path
+
+    sh -c "chmod +x iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.sh"
+
+    sh -c "nohup iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.sh > ../../iginx.log 2>&1 &"
+fi
