a5059174e6ef72933110ccb55cb17c0e08f2da6f
jzl18thu <jzl18thu@163.com>
2024-07-05 20:08:37 +0800

    Update iginx.sh

--- .github/scripts/iginx/iginx.sh
+++ .github/scripts/iginx/iginx.sh
@@ -20,19 +20,21 @@
 
 set -e
 
-sed -i "s/port=[0-9]\+/port=$1/g" '$3'/core/target/iginx-core-*/conf/config.properties
+cd "$3"
 
-sed -i "s/#iginx_port=[0-9]\+#/#iginx_port=$1#/g" '$3'/core/target/iginx-core-*/conf/config.properties
+sed -i "s/port=[0-9]\+/port=$1/g" core/target/iginx-core-*/conf/config.properties
 
-sed -i "s/restPort=[0-9]\+/restPort=$2/g" '$3'/core/target/iginx-core-*/conf/config.properties
+sed -i "s/#iginx_port=[0-9]\+#/#iginx_port=$1#/g" core/target/iginx-core-*/conf/config.properties
 
-sh -c "chmod +x $3/core/target/iginx-core-*/sbin/start_iginx.sh"
+sed -i "s/restPort=[0-9]\+/restPort=$2/g" core/target/iginx-core-*/conf/config.properties
 
-sh -c "nohup $3/core/target/iginx-core-*/sbin/start_iginx.sh > $3/iginx-$1.log 2>&1 &"
+sh -c "chmod +x core/target/iginx-core-*/sbin/start_iginx.sh"
+
+sh -c "nohup core/target/iginx-core-*/sbin/start_iginx.sh > iginx-$1.log 2>&1 &"
 
 sh -c "sleep 3"
 
-log_file="$3/iginx-$1.log"
+log_file="iginx-$1.log"
 timeout=30
 interval=2
 
