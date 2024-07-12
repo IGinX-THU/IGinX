722a7642e8972f8c4e25132ad381c6ff592918fd
jzl18thu <jzl18thu@163.com>
2024-07-12 12:23:20 +0800

    remove unsed debug info

--- dataSource/iotdb12/src/main/java/cn/edu/tsinghua/iginx/iotdb/IoTDBStorage.java
+++ dataSource/iotdb12/src/main/java/cn/edu/tsinghua/iginx/iotdb/IoTDBStorage.java
@@ -128,7 +128,7 @@ public class IoTDBStorage implements IStorage {
       session.open(false);
       session.close();
     } catch (IoTDBConnectionException e) {
-      LOGGER.debug("test connection error: ", e);
+      LOGGER.error("test connection error: ", e);
       return false;
     }
     return true;
@@ -176,7 +176,6 @@ public class IoTDBStorage implements IStorage {
         columnsInterval = new ColumnsInterval(dataPrefix);
       }
     } catch (IoTDBConnectionException | StatementExecutionException e) {
-      LOGGER.debug("get time series failure: ", e);
       throw new IoTDBTaskExecuteFailureException("get time series failure: ", e);
     }
 
@@ -245,7 +244,6 @@ public class IoTDBStorage implements IStorage {
       }
       dataSet.close();
     } catch (IoTDBConnectionException | StatementExecutionException e) {
-      LOGGER.debug("get time series failure: ", e);
       throw new IoTDBTaskExecuteFailureException("get time series failure: ", e);
     }
   }
@@ -317,7 +315,6 @@ public class IoTDBStorage implements IStorage {
                   sessionPool.executeQueryStatement(statement), true, project, filter));
       return new TaskExecuteResult(rowStream);
     } catch (IoTDBConnectionException | StatementExecutionException | PhysicalException e) {
-      LOGGER.debug("execute project task in iotdb12 failure, caused by: ", e);
       return new TaskExecuteResult(
           new IoTDBTaskExecuteFailureException("execute project task in iotdb12 failure", e));
     }
@@ -374,7 +371,6 @@ public class IoTDBStorage implements IStorage {
                   sessionPool.executeQueryStatement(statement), false, project, filter));
       return new TaskExecuteResult(rowStream);
     } catch (IoTDBConnectionException | StatementExecutionException | PhysicalException e) {
-      LOGGER.debug("execute project task in iotdb12 failure, caused by: ", e);
       return new TaskExecuteResult(
           new IoTDBTaskExecuteFailureException("execute project task in iotdb12 failure", e));
     }
@@ -400,7 +396,6 @@ public class IoTDBStorage implements IStorage {
         break;
     }
     if (e != null) {
-      LOGGER.debug("execute insert task in iotdb12 failure, caused by: ", e);
       return new TaskExecuteResult(
           null, new IoTDBException("execute insert task in iotdb12 failure", e));
     }
