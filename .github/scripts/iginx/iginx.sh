14dc90baa9d718c6b0f5ceb6608bd3a53507b4ef
jzl18thu <jzl18thu@163.com>
2024-07-14 20:19:59 +0800

    debug

--- .github/scripts/iginx/iginx.sh
+++ .github/scripts/iginx/iginx.sh
@@ -20,15 +20,17 @@
 
 set -e
 
-sed -i "s/port=[0-9]\+/port=$1/g" core/target/iginx-core-*/conf/config.properties
+echo "$PWD"
 
-sed -i "s/#iginx_port=[0-9]\+#/#iginx_port=$1#/g" core/target/iginx-core-*/conf/config.properties
+sed -i "s/port=[0-9]\+/port=$1/g" core/target/iginx-core-$3/conf/config.properties
 
-sed -i "s/restPort=[0-9]\+/restPort=$2/g" core/target/iginx-core-*/conf/config.properties
+sed -i "s/#iginx_port=[0-9]\+#/#iginx_port=$1#/g" core/target/iginx-core-$3/conf/config.properties
 
-sh -c "chmod +x core/target/iginx-core-*/sbin/start_iginx.sh"
+sed -i "s/restPort=[0-9]\+/restPort=$2/g" core/target/iginx-core-$3/conf/config.properties
 
-sh -c "nohup core/target/iginx-core-*/sbin/start_iginx.sh > iginx-$1.log 2>&1 &"
+sh -c "chmod +x core/target/iginx-core-$3/sbin/start_iginx.sh"
+
+sh -c "nohup core/target/iginx-core-$3/sbin/start_iginx.sh > iginx-$1.log 2>&1 &"
 
 sh -c "sleep 3"
 
