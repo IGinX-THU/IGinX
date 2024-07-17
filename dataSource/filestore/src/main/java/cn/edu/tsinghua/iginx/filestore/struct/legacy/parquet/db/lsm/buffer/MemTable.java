2aa781fb9d9ffb859e8bcc105f60e283f22b790e
An Qi <aqnin@outlook.com>
2024-07-17 15:14:50 +0800

    refactor(parquet): use nullable vector everywhere

--- dataSource/filestore/src/main/java/cn/edu/tsinghua/iginx/filestore/struct/legacy/parquet/db/lsm/buffer/MemTable.java
+++ dataSource/filestore/src/main/java/cn/edu/tsinghua/iginx/filestore/struct/legacy/parquet/db/lsm/buffer/MemTable.java
@@ -21,7 +21,6 @@ import cn.edu.tsinghua.iginx.filestore.struct.legacy.parquet.db.lsm.buffer.chunk
 import cn.edu.tsinghua.iginx.filestore.struct.legacy.parquet.db.lsm.buffer.chunk.IndexedChunk;
 import cn.edu.tsinghua.iginx.filestore.struct.legacy.parquet.db.lsm.table.MemoryTable;
 import cn.edu.tsinghua.iginx.filestore.struct.legacy.parquet.db.util.AreaSet;
-import cn.edu.tsinghua.iginx.filestore.struct.legacy.parquet.util.arrow.ArrowFields;
 import com.google.common.collect.RangeSet;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -121,10 +120,9 @@ public class MemTable implements AutoCloseable {
       if (closed) {
         throw new IllegalStateException("MemTable is closed");
       }
-      Field field = ArrowFields.nullable(data.getField());
       MemColumn column =
           columns.computeIfAbsent(
-              field,
+              data.getField(),
               key -> new MemColumn(factory, allocator, maxChunkValueCount, minChunkValueCount));
       column.store(data);
     } finally {
