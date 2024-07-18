fa971246d6afb80971c676c48b45b69d50b62ae2
jzl18thu <jzl18thu@163.com>
2024-07-18 23:37:01 +0800

    format

--- shared/src/test/java/cn/edu/tsinghua/iginx/utils/StringUtilsTest.java
+++ shared/src/test/java/cn/edu/tsinghua/iginx/utils/StringUtilsTest.java
@@ -19,7 +19,6 @@
 package cn.edu.tsinghua.iginx.utils;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collections;
