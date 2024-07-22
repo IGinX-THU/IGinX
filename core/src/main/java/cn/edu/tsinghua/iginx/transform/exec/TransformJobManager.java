b0da6814c93a29a3ca45e6bc6f6053421a89e9a8
ZM <12236590+shinyano@users.noreply.github.com>
2024-07-22 09:57:57 +0800

    feat(core): allow scheduling transform jobs (#366)
    Allow user to schedule their defined transform jobs with advanced options. Users can now specify the exact time for execution or set up recurring schedules.
    
    Usage:
    Add this line to the end of YAML file that defines your transform job.
    
    schedule: "<schedule_string>"
    <schedule_string> Formats
    The <schedule_string> can be in one of the following four formats:
    
    Recurring Execution:
    
    every 3 second/minute/hour/day/month/year: Executes every 3 seconds/minutes/hours/days/months/years.
    every 3 minute starts '2024-07-19 12:00:00' ends '2024-07-20 12:00:00': From 2024-07-19 12:00:00 to 2024-07-20 12:00:00, execute job every 3 minutes.
    every 3 minute ends '23:59:59': From now to 23:59:59 same day, execute job every 3 minutes.
    every 3 minute starts '13:00:00': From 13:00:00 today, execute job every 3 minutes forever.
    every mon,wed: Executes every Monday and Wednesday.
    Delayed Execution:
    
    after 3 second/minute/hour/day/month/year: Executes once after 3 seconds/minutes/hours/days/months/years.
    Specific Time Execution:
    
    at '2024-07-19 12:00:00': Executes once at the specified time.
    at '12:00:00': Executes once at the specified time today.
    Cron Format:
    
    A cron-formatted string.
    Cron string reference: Quartz cron trigger tutorials

--- core/src/main/java/cn/edu/tsinghua/iginx/transform/exec/TransformJobManager.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/transform/exec/TransformJobManager.java
@@ -25,6 +25,7 @@ import cn.edu.tsinghua.iginx.thrift.CommitTransformJobReq;
 import cn.edu.tsinghua.iginx.thrift.JobState;
 import cn.edu.tsinghua.iginx.thrift.TaskType;
 import cn.edu.tsinghua.iginx.transform.api.Checker;
+import cn.edu.tsinghua.iginx.transform.exception.TransformException;
 import cn.edu.tsinghua.iginx.transform.pojo.Job;
 import cn.edu.tsinghua.iginx.transform.pojo.PythonTask;
 import cn.edu.tsinghua.iginx.transform.pojo.Task;
@@ -107,20 +108,15 @@ public class TransformJobManager {
       runner.start();
       jobRunnerMap.put(job.getJobId(), runner);
       runner.run();
-      jobRunnerMap.remove(job.getJobId()); // since we will retry, we can't do this in finally
     } catch (Exception e) {
       LOGGER.error("Fail to process transform job id={}, because", job.getJobId(), e);
       throw e;
-    } finally {
-      // TODO: is it legal to retry after runner.close()???
-      // TODO:
-      // we don't need to close runner for FINISHED or FAILED jobs
-      // can we move runner.close() into catch clause?
-      runner.close();
     }
     // TODO: should we set end time and log time cost for failed jobs?
-    job.setEndTime(System.currentTimeMillis());
-    LOGGER.info("Job id={} cost {} ms.", job.getJobId(), job.getEndTime() - job.getStartTime());
+    if (!runner.scheduled()) {
+      job.setEndTime(System.currentTimeMillis());
+      LOGGER.info("Job id={} cost {} ms.", job.getJobId(), job.getEndTime() - job.getStartTime());
+    }
   }
 
   public boolean cancel(long jobId) {
@@ -147,22 +143,40 @@ public class TransformJobManager {
     switch (job.getState()) { // won't be null
       case JOB_RUNNING:
       case JOB_CREATED:
-        break; // continue execution
+        // atomic guard
+        if (!job.getActive().compareAndSet(true, false)) {
+          return false;
+        }
+      case JOB_IDLE:
+        // reorder as Normal run: [set-ING,] close, set-ED, remove[, set end time, log time cost].
+        job.setState(JobState.JOB_CLOSING);
+        runner.close();
+        job.setState(JobState.JOB_CLOSED);
+        jobRunnerMap.remove(jobId);
+        job.setEndTime(System.currentTimeMillis());
+        LOGGER.info("Job id={} cost {} ms.", job.getJobId(), job.getEndTime() - job.getStartTime());
+        return true;
       default:
         return false;
     }
-    // atomic guard
-    if (!job.getActive().compareAndSet(true, false)) {
-      return false;
+  }
+
+  public void removeFinishedScheduleJob(long jobID) throws TransformException {
+    Job job = jobMap.get(jobID);
+    if (job == null) {
+      throw new TransformException("No job with id: " + jobID + " exists.");
     }
-    // reorder as Normal run: [set-ING,] close, set-ED, remove[, set end time, log time cost].
-    job.setState(JobState.JOB_CLOSING);
-    runner.close();
-    job.setState(JobState.JOB_CLOSED);
-    jobRunnerMap.remove(jobId);
-    job.setEndTime(System.currentTimeMillis());
-    LOGGER.info("Job id={} cost {} ms.", job.getJobId(), job.getEndTime() - job.getStartTime());
-    return true;
+    JobRunner runner = jobRunnerMap.get(jobID);
+    if (runner == null) {
+      throw new TransformException("No job runner with id: " + jobID + " exists.");
+    }
+    if (job.getState() == JobState.JOB_FINISHED) {
+      jobRunnerMap.remove(jobID);
+      runner.close();
+      return;
+    }
+    throw new TransformException(
+        "Job with id: " + jobID + "did not finish correctly. Current state: " + job.getState());
   }
 
   public JobState queryJobState(long jobId) {
