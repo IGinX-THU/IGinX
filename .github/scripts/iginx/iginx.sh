984203ccae0f9e662695613c64dff6fd4b3873d5
aqni <aqnin@outlook.com>
2024-07-26 23:32:04 +0800

    revert test changes

--- .github/scripts/iginx/iginx.sh
+++ .github/scripts/iginx/iginx.sh
@@ -28,6 +28,27 @@ sed -i "s/restPort=[0-9]\+/restPort=$2/g" core/target/iginx-core-*/conf/config.p
 
 sh -c "chmod +x core/target/iginx-core-*/sbin/start_iginx.sh"
 
-sh -c "core/target/iginx-core-*/sbin/start_iginx.sh"
+sh -c "nohup core/target/iginx-core-*/sbin/start_iginx.sh > iginx-$1.log 2>&1 &"
+
+sh -c "sleep 3"
+
+log_file="iginx-$1.log"
+timeout=30
+interval=2
+
+elapsed_time=0
+while [ $elapsed_time -lt $timeout ]; do
+  last_lines=$(tail -n 20 "$log_file")
+  if echo "$last_lines" | grep -q "IGinX is now in service......"; then
+    echo "IGinX启动成功"
+    exit 0
+  fi
+  sleep $interval
+  elapsed_time=$((elapsed_time + interval))
+done
+
+echo "错误：IGinX未启动成功"
+
+sh -c "cat $log_file"
 
 exit 1
