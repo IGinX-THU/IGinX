460e2bf269ea3e3f3e98506675adf2683a682b50
An Qi <aqnin@outlook.com>
2024-07-26 16:21:39 +0800

    rollback classloader

--- core/src/main/java/cn/edu/tsinghua/iginx/engine/physical/storage/StorageManager.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/engine/physical/storage/StorageManager.java
@@ -157,7 +157,7 @@ public class StorageManager {
       String storage = kAndV[0];
       String driver = kAndV[1];
       try {
-        ClassLoader classLoader = StorageEngineClassLoader.of(storage);
+        ClassLoader classLoader = new StorageEngineClassLoader(storage);
         StorageEngineType type = StorageEngineType.valueOf(storage.toLowerCase());
         classLoaders.put(type, classLoader);
         drivers.put(type, driver);
