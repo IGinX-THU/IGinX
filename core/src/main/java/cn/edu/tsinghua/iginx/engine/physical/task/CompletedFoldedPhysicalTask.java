ecf617d830cbe87233e0a7114027ccb948e68522
An Qi <aqnin@outlook.com>
2024-10-16 15:01:07 +0800

    fix

--- core/src/main/java/cn/edu/tsinghua/iginx/engine/physical/task/CompletedFoldedPhysicalTask.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/engine/physical/task/CompletedFoldedPhysicalTask.java
@@ -22,10 +22,12 @@ import cn.edu.tsinghua.iginx.engine.physical.exception.PhysicalException;
 import cn.edu.tsinghua.iginx.engine.shared.RequestContext;
 import cn.edu.tsinghua.iginx.engine.shared.data.read.BatchStream;
 
+import java.util.Collections;
+
 public class CompletedFoldedPhysicalTask extends UnaryMemoryPhysicalTask {
 
   public CompletedFoldedPhysicalTask(PhysicalTask parentTask, RequestContext context) {
-    super(parentTask, context);
+    super(parentTask, Collections.emptyList(), context);
   }
 
   @Override
