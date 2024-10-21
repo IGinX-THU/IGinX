9dee92aaf03e971357cae654d47ee4b7a464d3e4
unknown <yuqing.zhu@139.com>
2024-10-21 17:36:03 +0800

    update license

--- client/src/assembly/resources/sbin/start_cli.bat
+++ client/src/assembly/resources/sbin/start_cli.bat
@@ -1,21 +1,22 @@
 @REM
 @REM IGinX - the polystore system with high performance
 @REM Copyright (C) Tsinghua University
+@REM TSIGinX@gmail.com
 @REM
-@REM This program is free software: you can redistribute it and/or modify
-@REM it under the terms of the GNU General Public License as published by
-@REM the Free Software Foundation, either version 3 of the License, or
-@REM (at your option) any later version.
+@REM This program is free software; you can redistribute it and/or
+@REM modify it under the terms of the GNU Lesser General Public
+@REM License as published by the Free Software Foundation; either
+@REM version 3 of the License, or (at your option) any later version.
 @REM
 @REM This program is distributed in the hope that it will be useful,
 @REM but WITHOUT ANY WARRANTY; without even the implied warranty of
-@REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-@REM GNU General Public License for more details.
+@REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+@REM Lesser General Public License for more details.
 @REM
-@REM You should have received a copy of the GNU General Public License
-@REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@REM You should have received a copy of the GNU Lesser General Public License
+@REM along with this program; if not, write to the Free Software Foundation,
+@REM Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 @REM
-
 
 @echo off
 
