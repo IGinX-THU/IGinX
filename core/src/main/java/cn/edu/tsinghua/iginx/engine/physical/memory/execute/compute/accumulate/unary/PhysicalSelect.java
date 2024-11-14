32fae0cc70b6b12e38ec7aabf340d906c922b313
An Qi <aqnin@outlook.com>
2024-11-14 17:16:26 +0800

    fix(arrow): max, min, first, last

--- core/src/main/java/cn/edu/tsinghua/iginx/engine/physical/memory/execute/compute/accumulate/unary/PhysicalSelect.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/engine/physical/memory/execute/compute/accumulate/unary/PhysicalSelect.java
@@ -93,7 +93,7 @@ public abstract class PhysicalSelect extends UnaryAccumulation {
         FieldVector vector, IntConsumer firstFoundConsumer, IntConsumer consumer) {
       int valueCount = vector.getValueCount();
       for (int i = 0; i < valueCount; i++) {
-        if (vector.isNull(i)) {
+        if (!vector.isNull(i)) {
           if (found) {
             consumer.accept(i);
           } else {
