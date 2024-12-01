8653a99de3147679fdfa4860b7ceca6249aae8de
chaos <958273999@qq.com>
2024-12-01 16:10:46 +0800

    1

--- dataSource/vectordb/src/main/java/cn/edu/tsinghua/iginx/vectordb/MilvusStorage.java
+++ dataSource/vectordb/src/main/java/cn/edu/tsinghua/iginx/vectordb/MilvusStorage.java
@@ -92,6 +92,7 @@ public class MilvusStorage implements IStorage {
    * @throws StorageInitializationException 如果存储引擎类型不匹配或初始化过程中发生错误。
    */
   public MilvusStorage(StorageEngineMeta meta) throws StorageInitializationException {
+    LOGGER.info("init milvus storage");
     if (!meta.getStorageEngine().equals(StorageEngineType.vectordb)) {
       throw new StorageInitializationException("unexpected database: " + meta.getStorageEngine());
     }
