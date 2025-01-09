8d0822d649c575a8084b34701f8aacad6a29ea32
An Qi <aqnin@outlook.com>
2025-01-09 13:02:01 +0800

    Revert "remove influxdb query limit"
    This reverts commit 07e59ae10e984e721791afebe0d41cc6fd108834.

--- .github/scripts/dataSources/startup/influxdb.sh
+++ .github/scripts/dataSources/startup/influxdb.sh
@@ -33,7 +33,7 @@ sh -c "cp -r $INFLUX_HOME/ influxdb2-2.0.7-linux-amd64"
 
 sh -c "ls influxdb2-2.0.7-linux-amd64"
 
-sudo sh -c "cd influxdb2-2.0.7-linux-amd64/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:8086 &"
+sudo sh -c "cd influxdb2-2.0.7-linux-amd64/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:8086 --query-memory-bytes=300971520 &"
 
 sh -c "sleep 30"
 
@@ -44,5 +44,5 @@ do
   # target path is also used in update/<db> script
   sh -c "sudo cp -r influxdb2-2.0.7-linux-amd64/ influxdb2-2.0.7-linux-amd64-$port/"
 
-  sudo sh -c "cd influxdb2-2.0.7-linux-amd64-$port/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:$port & echo \$! > influxdb.pid"
+  sudo sh -c "cd influxdb2-2.0.7-linux-amd64-$port/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:$port --query-memory-bytes=20971520 & echo \$! > influxdb.pid"
 done
