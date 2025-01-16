decc5cd56978feb80db0e16ceba6b754b5da0179
liuxtq <liu_xtq@163.com>
2025-01-16 16:17:02 +0800

    1.format

--- shared/src/main/java/cn/edu/tsinghua/iginx/utils/ShellRunner.java
+++ shared/src/main/java/cn/edu/tsinghua/iginx/utils/ShellRunner.java
@@ -35,7 +35,7 @@ public class ShellRunner {
     try {
       ProcessBuilder builder = new ProcessBuilder();
       if (isOnWin()) {
-        builder.command(BASH_PATH, command);// WSL负责安装docker环境,默认使用Git Bash,WSL Bash可能会缺少依赖的基础环境。
+        builder.command(BASH_PATH, command); // WSL负责安装docker环境,默认使用Git Bash,WSL Bash可能会缺少依赖的基础环境。
       } else {
         builder.command(command);
       }
