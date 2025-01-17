d03d28fd6bf2843569a4362cfd8eda0a383431ae
LeanMaX <149472644@qq.com>
2025-01-18 00:27:00 +0800

    fix: reformat code

--- dataSource/relational/src/main/java/cn/edu/tsinghua/iginx/relational/meta/JDBCMeta.java
+++ dataSource/relational/src/main/java/cn/edu/tsinghua/iginx/relational/meta/JDBCMeta.java
@@ -23,198 +23,199 @@ import cn.edu.tsinghua.iginx.metadata.entity.StorageEngineMeta;
 import cn.edu.tsinghua.iginx.relational.datatype.transformer.DmDataTypeTransformer;
 import cn.edu.tsinghua.iginx.relational.datatype.transformer.IDataTypeTransformer;
 import cn.edu.tsinghua.iginx.relational.datatype.transformer.JDBCDataTypeTransformer;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
 public class JDBCMeta extends AbstractRelationalMeta {
-  private final char quote;
-  private final String defaultDatabaseName;
+    private final char quote;
+    private final String defaultDatabaseName;
 
-  private final String driverClass;
+    private final String driverClass;
 
-  private final IDataTypeTransformer dataTypeTransformer;
+    private final IDataTypeTransformer dataTypeTransformer;
 
-  private final List<String> systemDatabaseName;
+    private final List<String> systemDatabaseName;
 
-  private final String databaseQuerySql;
+    private final String databaseQuerySql;
 
-  private final String databaseDropStatement;
+    private final String databaseDropStatement;
 
-  private final String databaseCreateStatement;
+    private final String databaseCreateStatement;
 
-  private final String grantPrivilegesStatement;
+    private final String grantPrivilegesStatement;
 
-  private final String createTableStatement;
+    private final String createTableStatement;
 
-  private final String alterTableAddColumnStatement;
+    private final String alterTableAddColumnStatement;
 
-  private final String alterTableDropColumnStatement;
+    private final String alterTableDropColumnStatement;
 
-  private final String queryTableStatement;
+    private final String queryTableStatement;
 
-  private final String queryTableWithoutKeyStatement;
+    private final String queryTableWithoutKeyStatement;
 
-  private final String updateTableStatement;
+    private final String updateTableStatement;
 
-  private final boolean needQuote;
+    private final boolean needQuote;
 
-  private final String schemaPattern;
-  private final String upsertStatement;
-  private final String upsertConflictStatement;
-  private final boolean isSupportFullJoin;
-  private final String regexpOp;
-  private final String notRegexOp;
-  private final boolean jdbcSupportBackslash;
+    private final String schemaPattern;
+    private final String upsertStatement;
+    private final String upsertConflictStatement;
+    private final boolean isSupportFullJoin;
+    private final String regexpOp;
+    private final String notRegexOp;
+    private final boolean jdbcSupportBackslash;
 
-  public JDBCMeta(StorageEngineMeta meta, Properties properties) {
-    super(meta);
-    quote = properties.getProperty("quote").charAt(0);
-    driverClass = properties.getProperty("driver_class");
-    defaultDatabaseName = properties.getProperty("default_database");
-    if (meta.getExtraParams().get("engine").equalsIgnoreCase("dm")) {
-      dataTypeTransformer = DmDataTypeTransformer.getInstance();
-    } else {
-      dataTypeTransformer = new JDBCDataTypeTransformer(properties);
+    public JDBCMeta(StorageEngineMeta meta, Properties properties) {
+        super(meta);
+        quote = properties.getProperty("quote").charAt(0);
+        driverClass = properties.getProperty("driver_class");
+        defaultDatabaseName = properties.getProperty("default_database");
+        if (meta.getExtraParams().get("engine").equalsIgnoreCase("dm")) {
+            dataTypeTransformer = DmDataTypeTransformer.getInstance();
+        } else {
+            dataTypeTransformer = new JDBCDataTypeTransformer(properties);
+        }
+        systemDatabaseName = Arrays.asList(properties.getProperty("system_databases").split(","));
+        databaseQuerySql = properties.getProperty("database_query_sql");
+        databaseDropStatement = properties.getProperty("drop_database_statement");
+        databaseCreateStatement = properties.getProperty("create_database_statement");
+        grantPrivilegesStatement = properties.getProperty("grant_privileges_statement");
+        createTableStatement = properties.getProperty("create_table_statement");
+        alterTableAddColumnStatement = properties.getProperty("alter_table_add_column_statement");
+        alterTableDropColumnStatement = properties.getProperty("alter_table_drop_column_statement");
+        queryTableStatement = properties.getProperty("query_table_statement");
+        queryTableWithoutKeyStatement = properties.getProperty("query_table_without_key_statement");
+        updateTableStatement = properties.getProperty("update_table_statement");
+        needQuote = Boolean.parseBoolean(properties.getProperty("jdbc_need_quote"));
+        schemaPattern = properties.getProperty("schema_pattern");
+        upsertStatement = properties.getProperty("upsert_statement");
+        upsertConflictStatement = properties.getProperty("upsert_conflict_statement");
+        isSupportFullJoin = Boolean.parseBoolean(properties.getProperty("is_support_full_join"));
+        regexpOp = properties.getProperty("regex_like_symbol");
+        notRegexOp = properties.getProperty("not_regex_like_symbol");
+        jdbcSupportBackslash =
+                Boolean.parseBoolean(properties.getProperty("jdbc_support_special_char"));
     }
-    systemDatabaseName = Arrays.asList(properties.getProperty("system_databases").split(","));
-    databaseQuerySql = properties.getProperty("database_query_sql");
-    databaseDropStatement = properties.getProperty("drop_database_statement");
-    databaseCreateStatement = properties.getProperty("create_database_statement");
-    grantPrivilegesStatement = properties.getProperty("grant_privileges_statement");
-    createTableStatement = properties.getProperty("create_table_statement");
-    alterTableAddColumnStatement = properties.getProperty("alter_table_add_column_statement");
-    alterTableDropColumnStatement = properties.getProperty("alter_table_drop_column_statement");
-    queryTableStatement = properties.getProperty("query_table_statement");
-    queryTableWithoutKeyStatement = properties.getProperty("query_table_without_key_statement");
-    updateTableStatement = properties.getProperty("update_table_statement");
-    needQuote = Boolean.parseBoolean(properties.getProperty("jdbc_need_quote"));
-    schemaPattern = properties.getProperty("schema_pattern");
-    upsertStatement = properties.getProperty("upsert_statement");
-    upsertConflictStatement = properties.getProperty("upsert_conflict_statement");
-    isSupportFullJoin = Boolean.parseBoolean(properties.getProperty("is_support_full_join"));
-    regexpOp = properties.getProperty("regex_like_symbol");
-    notRegexOp = properties.getProperty("not_regex_like_symbol");
-    jdbcSupportBackslash =
-        Boolean.parseBoolean(properties.getProperty("jdbc_support_special_char"));
-  }
 
-  @Override
-  public char getQuote() {
-    return quote;
-  }
+    @Override
+    public char getQuote() {
+        return quote;
+    }
 
-  @Override
-  public String getDefaultDatabaseName() {
-    return defaultDatabaseName;
-  }
+    @Override
+    public String getDefaultDatabaseName() {
+        return defaultDatabaseName;
+    }
 
-  @Override
-  public String getDriverClass() {
-    return driverClass;
-  }
+    @Override
+    public String getDriverClass() {
+        return driverClass;
+    }
 
-  @Override
-  public IDataTypeTransformer getDataTypeTransformer() {
-    return dataTypeTransformer;
-  }
+    @Override
+    public IDataTypeTransformer getDataTypeTransformer() {
+        return dataTypeTransformer;
+    }
 
-  @Override
-  public List<String> getSystemDatabaseName() {
-    return systemDatabaseName;
-  }
+    @Override
+    public List<String> getSystemDatabaseName() {
+        return systemDatabaseName;
+    }
 
-  @Override
-  public String getDatabaseQuerySql() {
-    return databaseQuerySql;
-  }
+    @Override
+    public String getDatabaseQuerySql() {
+        return databaseQuerySql;
+    }
 
-  @Override
-  public String getDropDatabaseStatement() {
-    return databaseDropStatement;
-  }
+    @Override
+    public String getDropDatabaseStatement() {
+        return databaseDropStatement;
+    }
 
-  @Override
-  public String getCreateDatabaseStatement() {
-    return databaseCreateStatement;
-  }
+    @Override
+    public String getCreateDatabaseStatement() {
+        return databaseCreateStatement;
+    }
 
-  @Override
-  public String getGrantPrivilegesStatement() {
-    return grantPrivilegesStatement;
-  }
+    @Override
+    public String getGrantPrivilegesStatement() {
+        return grantPrivilegesStatement;
+    }
 
-  //  @Override
-  //  public String getCreateTableStatement() {
-  //    return createTableStatement;
-  //  }
+    //  @Override
+    //  public String getCreateTableStatement() {
+    //    return createTableStatement;
+    //  }
 
-  @Override
-  public String getAlterTableDropColumnStatement() {
-    return alterTableDropColumnStatement;
-  }
+    @Override
+    public String getAlterTableDropColumnStatement() {
+        return alterTableDropColumnStatement;
+    }
 
-  @Override
-  public String getAlterTableAddColumnStatement() {
-    return alterTableAddColumnStatement;
-  }
+    @Override
+    public String getAlterTableAddColumnStatement() {
+        return alterTableAddColumnStatement;
+    }
 
-  @Override
-  public String getQueryTableStatement() {
-    return queryTableStatement;
-  }
+    @Override
+    public String getQueryTableStatement() {
+        return queryTableStatement;
+    }
 
-  @Override
-  public String getQueryTableWithoutKeyStatement() {
-    return queryTableWithoutKeyStatement;
-  }
+    @Override
+    public String getQueryTableWithoutKeyStatement() {
+        return queryTableWithoutKeyStatement;
+    }
 
-  @Override
-  public String getUpdateTableStatement() {
-    return updateTableStatement.isEmpty() ? getUpdateStatement() : updateTableStatement;
-  }
+    @Override
+    public String getUpdateTableStatement() {
+        return updateTableStatement.isEmpty() ? getUpdateStatement() : updateTableStatement;
+    }
 
-  @Override
-  public boolean jdbcNeedQuote() {
-    return needQuote;
-  }
+    @Override
+    public boolean jdbcNeedQuote() {
+        return needQuote;
+    }
 
-  @Override
-  public String getSchemaPattern() {
-    return schemaPattern;
-  }
+    @Override
+    public String getSchemaPattern() {
+        return schemaPattern;
+    }
 
-  @Override
-  public String getUpsertStatement() {
-    return upsertStatement;
-  }
+    @Override
+    public String getUpsertStatement() {
+        return upsertStatement;
+    }
 
-  @Override
-  public String getUpsertConflictStatement() {
-    return upsertConflictStatement;
-  }
+    @Override
+    public String getUpsertConflictStatement() {
+        return upsertConflictStatement;
+    }
 
-  @Override
-  public boolean isSupportFullJoin() {
-    return isSupportFullJoin;
-  }
+    @Override
+    public boolean isSupportFullJoin() {
+        return isSupportFullJoin;
+    }
 
-  @Override
-  public String getRegexpOp() {
-    return regexpOp;
-  }
+    @Override
+    public String getRegexpOp() {
+        return regexpOp;
+    }
 
-  @Override
-  public String getNotRegexpOp() {
-    return notRegexOp;
-  }
+    @Override
+    public String getNotRegexpOp() {
+        return notRegexOp;
+    }
 
-  @Override
-  public boolean jdbcSupportSpecialChar() {
-    return jdbcSupportBackslash;
-  }
+    @Override
+    public boolean jdbcSupportSpecialChar() {
+        return jdbcSupportBackslash;
+    }
 
-  public StorageEngineMeta getStorageEngineMeta() {
-    return meta;
-  }
+    public StorageEngineMeta getStorageEngineMeta() {
+        return meta;
+    }
 }
