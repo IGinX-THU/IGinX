cc548dc49930de838f92abb36114614d43d08717
jzl18thu <jzl18thu@163.com>
2025-01-20 13:16:28 +0800

    test

--- .github/scripts/iginx/iginx_macos.sh
+++ .github/scripts/iginx/iginx_macos.sh
@@ -28,7 +28,7 @@ sed -i "" -E "s/restPort=[0-9]+/restPort=$2/g" core/target/iginx-core-*/conf/con
 
 sh -c "chmod +x core/target/iginx-core-*/sbin/start_iginx.sh"
 
-sh -c "nohup core/target/iginx-core-*/sbin/start_iginx.sh > iginx-$1.log 2>&1 &"
+sudo -E sh -c "nohup core/target/iginx-core-*/sbin/start_iginx.sh > iginx-$1.log 2>&1 &"
 
 sh -c "sleep 3"
 
