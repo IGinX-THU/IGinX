911b29608bb550ba299b1fba40f8c863ad3d5d45
ZM <12236590+shinyano@users.noreply.github.com>
2025-01-20 11:51:43 +0800

    feat(ci): test free-threading python 3.13.0 (#494)
    Add tests for python3.13t on:
    win & mac & linux
    iotdb & filesystem
    
    Note:
    The testAddStorageEngine test in PySessionIT is skipped because it relies on fastparquet module which does not support python 3.13 yet
    
    顺便解决IoTDB启动时使用了非JDK8导致出错的问题
    剩余一个3.13t测试在macOS、FS情况下出错的问题，在PR#555中解决

--- .github/scripts/dataSources/restart/influxdb_macos.sh
+++ .github/scripts/dataSources/restart/influxdb_macos.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # IGinX - the polystore system with high performance
 # Copyright (C) Tsinghua University
@@ -22,5 +22,10 @@
 
 port=$1
 echo "Starting InfluxDB on port $port"
-sudo sh -c "cd influxdb2-2.0.7-darwin-amd64-$port/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:$port --query-memory-bytes=20971520 --query-concurrency=2 > influxdb_$port.log 2>&1 &"
+sudo -E sh -c "cd influxdb2-2.0.7-darwin-amd64-$port/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:$port --query-memory-bytes=300971520 --query-concurrency=2 > influxdb_$port.log 2>&1 &"
 sleep 10
+
+sudo lsof -i:$port
+if [ $? -eq 1 ]; then
+    echo "No process is listening on port $port"
+fi
