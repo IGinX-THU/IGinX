5f1b80db60ae65b6aa40f387a2c27cfe8a6e4c72
LeanMaX <149472644@qq.com>
2025-01-24 13:27:39 +0800

    fix: fix DamengHistorydataGenerator

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/DamengHistoryDataGenerator.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/DamengHistoryDataGenerator.java
@@ -1,12 +1,30 @@
+/*
+ * IGinX - the polystore system with high performance
+ * Copyright (C) Tsinghua University
+ * TSIGinX@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package cn.edu.tsinghua.iginx.integration.expansion;
 
 import cn.edu.tsinghua.iginx.integration.expansion.constant.Constant;
 import cn.edu.tsinghua.iginx.thrift.DataType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.sql.*;
 import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DamengHistoryDataGenerator extends BaseHistoryDataGenerator {
 
@@ -15,22 +33,17 @@ public class DamengHistoryDataGenerator extends BaseHistoryDataGenerator {
   private static final char SEPARATOR = '.';
 
   private static final String QUERY_DATABASES_STATEMENT =
-          "SELECT SCHEMA_NAME FROM DBA_SCHEMAS WHERE SCHEMA_NAME NOT IN ('SYSDBA', 'SYSAUDITOR')";
+      "SELECT SCHEMA_NAME FROM DBA_SCHEMAS WHERE SCHEMA_NAME NOT IN ('SYSDBA', 'SYSAUDITOR')";
 
-  private static final String CREATE_DATABASE_STATEMENT =
-          "CREATE SCHEMA %s";
+  private static final String CREATE_DATABASE_STATEMENT = "CREATE SCHEMA %s";
 
-  private static final String GRANT_DATABASE_STATEMENT =
-          "GRANT CONNECT, RESOURCE TO %s";
+  private static final String GRANT_DATABASE_STATEMENT = "GRANT CONNECT, RESOURCE TO %s";
 
-  private static final String CREATE_TABLE_STATEMENT =
-          "CREATE TABLE %s.%s (%s)";
+  private static final String CREATE_TABLE_STATEMENT = "CREATE TABLE %s.%s (%s)";
 
-  private static final String INSERT_STATEMENT =
-          "INSERT INTO %s.%s VALUES %s";
+  private static final String INSERT_STATEMENT = "INSERT INTO %s.%s VALUES %s";
 
-  private static final String DROP_DATABASE_STATEMENT =
-          "DROP SCHEMA %s CASCADE";
+  private static final String DROP_DATABASE_STATEMENT = "DROP SCHEMA %s CASCADE";
 
   public DamengHistoryDataGenerator() {
     Constant.oriPort = 5236;
@@ -40,9 +53,9 @@ public class DamengHistoryDataGenerator extends BaseHistoryDataGenerator {
 
   private Connection connect(int port, boolean useSystemDatabase, String databaseName) {
     try {
-      String url = String.format(
-              "jdbc:dm://172.17.0.2:%d/%s",
-              port, databaseName == null ? "DMDB" : databaseName);
+      String url =
+          String.format(
+              "jdbc:dm://172.17.0.2:%d/%s", port, databaseName == null ? "DMDB" : databaseName);
       Class.forName("dm.jdbc.driver.DmDriver");
       return DriverManager.getConnection(url, "SYSDBA", "SYSDBA001");
     } catch (SQLException | ClassNotFoundException e) {
@@ -52,11 +65,11 @@ public class DamengHistoryDataGenerator extends BaseHistoryDataGenerator {
 
   @Override
   public void writeHistoryData(
-          int port,
-          List<String> pathList,
-          List<DataType> dataTypeList,
-          List<Long> keyList,
-          List<List<Object>> valuesList) {
+      int port,
+      List<String> pathList,
+      List<DataType> dataTypeList,
+      List<Long> keyList,
+      List<List<Object>> valuesList) {
     Connection connection = null;
     try {
       connection = connect(port, true, null);
@@ -72,21 +85,21 @@ public class DamengHistoryDataGenerator extends BaseHistoryDataGenerator {
         String tableName = path.substring(path.indexOf(SEPARATOR) + 1, path.lastIndexOf(SEPARATOR));
 
         Map<String, List<Integer>> tablesToColumnIndexes =
-                databaseToTablesToColumnIndexes.computeIfAbsent(databaseName, x -> new HashMap<>());
+            databaseToTablesToColumnIndexes.computeIfAbsent(databaseName, x -> new HashMap<>());
         List<Integer> columnIndexes =
-                tablesToColumnIndexes.computeIfAbsent(tableName, x -> new ArrayList<>());
+            tablesToColumnIndexes.computeIfAbsent(tableName, x -> new ArrayList<>());
         columnIndexes.add(i);
         tablesToColumnIndexes.put(tableName, columnIndexes);
       }
 
       for (Map.Entry<String, Map<String, List<Integer>>> entry :
-              databaseToTablesToColumnIndexes.entrySet()) {
+          databaseToTablesToColumnIndexes.entrySet()) {
         String databaseName = entry.getKey();
         Statement stmt = connection.createStatement();
         String createDatabaseSql =
-                String.format(CREATE_DATABASE_STATEMENT, getQuotName(databaseName));
+            String.format(CREATE_DATABASE_STATEMENT, getQuotName(databaseName));
         String grantDatabaseSql =
-                String.format(GRANT_DATABASE_STATEMENT, getQuotName(databaseName));
+            String.format(GRANT_DATABASE_STATEMENT, getQuotName(databaseName));
         try {
           LOGGER.info("create database with stmt: {}", createDatabaseSql);
           stmt.execute(createDatabaseSql);
@@ -105,15 +118,19 @@ public class DamengHistoryDataGenerator extends BaseHistoryDataGenerator {
             String path = pathList.get(index);
             String columnName = path.substring(path.lastIndexOf(SEPARATOR) + 1);
             DataType dataType = dataTypeList.get(index);
-            createTableStr.append(getQuotName(columnName)).append(" ").append(toDamengSQL(dataType)).append(", ");
+            createTableStr
+                .append(getQuotName(columnName))
+                .append(" ")
+                .append(toDamengSQL(dataType))
+                .append(", ");
             columnStr.append(getQuotName(columnName)).append(",");
           }
           stmt.execute(
-                  String.format(
-                          CREATE_TABLE_STATEMENT,
-                          getQuotName(databaseName),
-                          getQuotName(tableName),
-                          createTableStr.substring(0, createTableStr.length() - 2)));
+              String.format(
+                  CREATE_TABLE_STATEMENT,
+                  getQuotName(databaseName),
+                  getQuotName(tableName),
+                  createTableStr.substring(0, createTableStr.length() - 2)));
         }
         stmt.close();
       }
@@ -128,6 +145,64 @@ public class DamengHistoryDataGenerator extends BaseHistoryDataGenerator {
         }
       } catch (SQLException e) {
         LOGGER.error("close connection failure: ", e);
+      }
+    }
+  }
+
+  @Override
+  public void writeHistoryData(
+      int port, List<String> pathList, List<DataType> dataTypeList, List<List<Object>> valuesList) {
+    writeHistoryData(port, pathList, dataTypeList, new ArrayList<>(), valuesList);
+  }
+
+  @Override
+  public void writeSpecialHistoryData() {
+    // write float value
+    writeHistoryData(
+        Constant.readOnlyPort,
+        Constant.READ_ONLY_FLOAT_PATH_LIST,
+        new ArrayList<>(Collections.singletonList(DataType.FLOAT)),
+        Constant.READ_ONLY_FLOAT_VALUES_LIST);
+  }
+
+  @Override
+  public void clearHistoryDataForGivenPort(int port) {
+    Connection conn = null;
+    try {
+      conn = connect(port, true, null);
+      Statement stmt = conn.createStatement();
+
+      // 查询达梦数据库的所有数据库
+      ResultSet databaseSet = stmt.executeQuery("SELECT NAME FROM SYS.DATABASES");
+      Statement dropDatabaseStatement = conn.createStatement();
+
+      while (databaseSet.next()) {
+        String databaseName = databaseSet.getString("NAME");
+
+        // 先终止所有连接，才能删除数据库
+        dropDatabaseStatement.addBatch(
+            String.format("DROP DATABASE %s FORCE", getQuotName(databaseName)));
+
+        LOGGER.info("Dropping database {} on 127.0.0.1:{}...", databaseName, port);
+      }
+
+      dropDatabaseStatement.executeBatch();
+
+      dropDatabaseStatement.close();
+      databaseSet.close();
+      stmt.close();
+      conn.close();
+      LOGGER.info("Successfully cleared history data on 127.0.0.1:{}!", port);
+
+    } catch (SQLException e) {
+      LOGGER.warn("Failed to clear history data on 127.0.0.1:{}: ", port, e);
+    } finally {
+      try {
+        if (conn != null) {
+          conn.close();
+        }
+      } catch (SQLException e) {
+        LOGGER.error("Failed to close connection: ", e);
       }
     }
   }
