6d75b48f58d0a666c06aea4b7b43a0a63572bb63
aqni <aqnin@outlook.com>
2025-02-21 10:30:04 +0800

    fix: revert incorrect modifications

--- .github/scripts/test/cli/test_job_register_macos.sh
+++ .github/scripts/test/cli/test_job_register_macos.sh
@@ -16,18 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-
+ 
 
 set -e
 
-<<<<<<<< HEAD:.github/scripts/dataSources/restart/postgresql.sh
-PGDATA=$1
-
-nohup pg_ctl -D ".github/actions/service/postgresql/${PGDATA}" start > /dev/null 2>&1 &
-
-sleep 3
-lsof -i:$1
-========
 cp -f test/src/test/resources/transform/TransformMultiplePythonJobsWithExportToIginx.yaml client/target/iginx-client-$1/sbin/TransformMultiplePythonJobsWithExportToIginx.yaml
 
 ls client/target/iginx-client-$1/sbin
@@ -48,4 +40,3 @@ else
   echo $result
   exit 1
 fi
->>>>>>>> refs/heads/main:.github/scripts/test/cli/test_job_register_macos.sh
