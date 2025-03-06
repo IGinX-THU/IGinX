4a925436d1fcf60534f15d4272be9a8d7ac441c0
LeanMaX <149472644@qq.com>
2025-03-06 23:01:18 +0800

    add update of dameng

--- .github/scripts/dataSources/update/dameng.sh
+++ .github/scripts/dataSources/update/dameng.sh
@@ -1,2 +1,2 @@
 # 进入docker容器并执行命令
-docker exec -it dm8-$1 /bin/bash -c "cd /opt/dmdbms/bin && ./disql SYSDBA/$2 -e 'ALTER USER SYSDBA IDENTIFIED BY \"$3\";'"
+docker exec -i dm8-$1 /bin/bash -c "cd /opt/dmdbms/bin && ./disql SYSDBA/$2 -e \"ALTER USER SYSDBA IDENTIFIED BY \\\"$3\\\";\"" || echo "执行失败，请检查参数和数据库状态"
