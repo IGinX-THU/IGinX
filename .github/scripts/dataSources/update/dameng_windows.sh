cc396f9d07e426b2a72d6a5f324ba1d157b2fa49
Lean <8209210614@csu.edu.cn>
2025-03-09 14:06:15 +0800

    fix readonly of dameng in windows

--- .github/scripts/dataSources/update/dameng_windows.sh
+++ .github/scripts/dataSources/update/dameng_windows.sh
@@ -1,4 +1,4 @@
-docker ps -a
-docker ps
+wsl docker ps -a
+wsl docker ps
 # 进入docker容器并执行命令
-docker exec -i dm8-$1 /bin/bash -c "cd /opt/dmdbms/bin && ./disql SYSDBA/$2 -e \"ALTER USER SYSDBA IDENTIFIED BY \\\"$3\\\";\"" || echo "执行失败，请检查参数和数据库状态"
+wsl docker exec -i dm8-$1 /bin/bash -c "cd /opt/dmdbms/bin && ./disql SYSDBA/$2 -e \"ALTER USER SYSDBA IDENTIFIED BY \\\"$3\\\";\"" || echo "执行失败，请检查参数和数据库状态"
