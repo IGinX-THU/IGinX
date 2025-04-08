aef97571914d690c3901a5a528d66caa7dc35f59
An Qi <aqnin@outlook.com>
2025-04-08 15:50:04 +0800

    test: decrease warmup records

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/warmup/SQLWarmupIT.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/warmup/SQLWarmupIT.java
@@ -41,7 +41,7 @@ public class SQLWarmupIT implements AutoCloseable {
 
   private static final String SQL = System.getProperty("iginx.warmup.sql", "warmup/warmup.sql");
   private static final int WARMUP = Integer.getInteger("iginx.warmup.repeat", 3);
-  private static final int RECORD = Integer.getInteger("iginx.warmup.records", 100000);
+  private static final int RECORD = Integer.getInteger("iginx.warmup.records", 50000);
   private static final String IP = System.getProperty("iginx.warmup.ip", "127.0.0.1");
   private static final int PORT = Integer.getInteger("iginx.warmup.port", 6888);
   private static final String USERNAME = System.getProperty("iginx.warmup.username", "root");
