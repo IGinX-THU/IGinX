bc7d95344446ddc159fb2043e02af667be58b986
reamofchaos <reamofchaos@163.com>
2025-04-11 09:45:15 +0800

    add neo4j database

---
+++ .github/scripts/dataSources/shutdown/neo4j.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# IGinX - the polystore system with high performance
+# Copyright (C) Tsinghua University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+port=$1
+pwd
+./neo4j_instances/$port/bin/neo4j stop
+sleep 5
+
+if netstat -an | grep -q ".*:$port.*LISTEN"; then
+  echo "Port $port is open."
+else
+  echo "Port $port is not open."
+fi
---
+++ dataSource/neo4j/src/main/java/cn/edu/tsinghua/iginx/neo4j/tools/Neo4jSchema.java
@@ -0,0 +1,77 @@
+/*
+ * IGinX - the polystore system with high performance
+ * Copyright (C) Tsinghua University
+ * TSIGinX@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package cn.edu.tsinghua.iginx.neo4j.tools;
+
+import static cn.edu.tsinghua.iginx.constant.GlobalConstant.SEPARATOR;
+
+public class Neo4jSchema {
+
+  private final String databaseName;
+
+  private final String labelName;
+
+  private final String propertyName;
+
+  private static final char quote = '`';
+
+  public Neo4jSchema(String path) {
+    this(path, false);
+  }
+
+  public Neo4jSchema(String path, boolean isDummy) {
+    databaseName = "";
+    int lastSeparator = path.lastIndexOf(".");
+    labelName = path.substring(0, lastSeparator);
+    propertyName = path.substring(lastSeparator + 1);
+  }
+
+  public String getDatabaseName() {
+    return databaseName;
+  }
+
+  public String getLabelName() {
+    return labelName;
+  }
+
+  public String getPropertyName() {
+    return propertyName;
+  }
+
+  private static String getQuoteName(String name, char quote) {
+    return quote + name + quote;
+  }
+
+  public static String getQuoteName(String name) {
+    name = name.replaceFirst("^" + quote, "").replaceFirst(quote + "$", "");
+    return quote + name + quote;
+  }
+
+  public static String getQuoteFullName(String labelName, String propertyName) {
+    return getQuoteName(labelName) + SEPARATOR + getQuoteName(propertyName);
+  }
+
+  public String getFullName() {
+    return getQuoteFullName(labelName, propertyName);
+  }
+
+  public static String getFullName(String tableName, String columnName) {
+    return tableName + SEPARATOR + columnName;
+  }
+}
