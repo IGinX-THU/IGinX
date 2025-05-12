368417cb08b7692198520ff9c74773b786c16947
jzl18thu <jzl18thu@163.com>
2025-05-12 15:59:05 +0800

    fix

--- .github/scripts/iginx/iginx_windows.sh
+++ .github/scripts/iginx/iginx_windows.sh
@@ -26,7 +26,7 @@ sed -i "s/#iginx_port=[0-9]\+#/#iginx_port=$1#/g" $3/conf/config.properties
 
 sed -i "s/restPort=[0-9]\+/restPort=$2/g" $3/conf/config.properties
 
-batPath="$(find core/target -name 'start_iginx.bat' | grep 'iginx-core-.*\/sbin' | head -n 1)"
+batPath="$3/sbin/start_iginx.bat"
 
 sed -i 's/-Xmx%MAX_HEAP_SIZE% -Xms%MAX_HEAP_SIZE%/-Xmx4g -Xms4g -XX:MaxMetaspaceSize=512M/g' $batPath
 
