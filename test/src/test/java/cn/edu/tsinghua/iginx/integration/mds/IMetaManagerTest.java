e7de7fbfbe1741e0d1b705ce207dae2154f6332a
An Qi <aqnin@outlook.com>
2025-05-20 17:48:15 +0800

    test(metadata): special character  in ColumnInterval of FragmentMeta

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/mds/IMetaManagerTest.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/mds/IMetaManagerTest.java
@@ -19,24 +19,30 @@
  */
 package cn.edu.tsinghua.iginx.integration.mds;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import cn.edu.tsinghua.iginx.conf.ConfigDescriptor;
 import cn.edu.tsinghua.iginx.metadata.DefaultMetaManager;
 import cn.edu.tsinghua.iginx.metadata.IMetaManager;
+import cn.edu.tsinghua.iginx.metadata.entity.ColumnsInterval;
+import cn.edu.tsinghua.iginx.metadata.entity.FragmentMeta;
+import cn.edu.tsinghua.iginx.metadata.entity.KeyInterval;
 import cn.edu.tsinghua.iginx.metadata.entity.StorageEngineMeta;
 import cn.edu.tsinghua.iginx.thrift.StorageEngineType;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.junit.*;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class IMetaManagerTest {
 
   private static IMetaManager iMetaManager;
 
-  @BeforeClass
+  @BeforeAll
   public static void beforeClass() {
     if (System.getenv("STORAGE") != null) {
       switch (System.getenv("STORAGE")) {
@@ -60,15 +66,12 @@ public class IMetaManagerTest {
     iMetaManager = DefaultMetaManager.getInstance();
   }
 
-  @AfterClass
+  @AfterAll
   public static void afterClass() {
     iMetaManager = null;
   }
 
-  @Before
-  public void setUp() {}
-
-  @After
+  @AfterEach
   public void tearDown() {}
 
   @Test
@@ -130,5 +133,36 @@ public class IMetaManagerTest {
   @Test
   public void storageUnitAndFragmentTest() {
     // TODO: 测试优先级
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideColumnsIntervals")
+  public void specialCharInColumnIntervalOfFragmentMeta(ColumnsInterval interval) throws Exception {
+    FragmentMeta fragmentMeta =
+        new FragmentMeta(interval, KeyInterval.getDefaultKeyInterval(), "test");
+    iMetaManager.addFragment(fragmentMeta);
+    try {
+      long pointsCount = System.identityHashCode(fragmentMeta);
+      iMetaManager.updateFragmentPoints(fragmentMeta, pointsCount);
+      Map<FragmentMeta, Long> points = iMetaManager.loadFragmentPoints();
+      assertEquals(pointsCount, points.get(fragmentMeta).longValue());
+    } finally {
+      iMetaManager.removeFragment(fragmentMeta);
+    }
+  }
+
+  private static Stream<ColumnsInterval> provideColumnsIntervals() {
+    return Stream.concat(
+        Stream.of(
+            new ColumnsInterval("start column", "end column"), // 正常情况
+            new ColumnsInterval(null, null), // 两端为 null
+            new ColumnsInterval("null", "null"), // 两端为 null
+            new ColumnsInterval("start column", null), // 仅结束列为 null
+            new ColumnsInterval(null, "end column"), // 仅开始列为 null
+            new ColumnsInterval("", ""), // 空字符串
+            new ColumnsInterval(" ", " "), // 空格
+            new ColumnsInterval("特殊字符.测试", "结束列.测试") // Unicode 字符
+            ),
+        "-\\".chars().mapToObj(c -> new ColumnsInterval("start" + (char) c, "end" + (char) c)));
   }
 }
