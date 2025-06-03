0439e9e0551f92a2934dcb14b7bea0e9f1312c37
An Qi <aqnin@outlook.com>
2025-06-04 00:22:27 +0800

    Revert "ci: less query concurrency of influxdb server on mac"
    This reverts commit 3019df73

--- .github/scripts/dataSources/startup/influxdb_macos.sh
+++ .github/scripts/dataSources/startup/influxdb_macos.sh
@@ -33,7 +33,7 @@ sh -c "cp -r $INFLUX_HOME/ influxdb2-2.0.7-darwin-amd64"
 
 sh -c "ls influxdb2-2.0.7-darwin-amd64"
 
-sudo sh -c "cd influxdb2-2.0.7-darwin-amd64/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:8086 --query-memory-bytes=300971520 --query-concurrency=1 &"
+sudo sh -c "cd influxdb2-2.0.7-darwin-amd64/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:8086 --query-memory-bytes=300971520 --query-concurrency=2 &"
 
 sh -c "sleep 30"
 
@@ -44,7 +44,7 @@ do
   # target path is also used in update/<db> script
   sh -c "sudo cp -r influxdb2-2.0.7-darwin-amd64/ influxdb2-2.0.7-darwin-amd64-$port/"
 
-  sudo -E sh -c "cd influxdb2-2.0.7-darwin-amd64-$port/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:$port --query-memory-bytes=300971520 --query-concurrency=1 & echo \$! > influxdb.pid"
+  sudo -E sh -c "cd influxdb2-2.0.7-darwin-amd64-$port/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:$port --query-memory-bytes=300971520 --query-concurrency=2 & echo \$! > influxdb.pid"
 done
 
 sh -c "sleep 30"
