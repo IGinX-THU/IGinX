8f1858aa2ea497a5aa1a578bdd88147903da1606
shinyano <12236590+shinyano@users.noreply.github.com>
2025-06-04 22:38:23 +0800

    restore & format

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/func/udf/UDFIT.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/func/udf/UDFIT.java
@@ -47,7 +47,6 @@ import org.junit.AfterClass;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
